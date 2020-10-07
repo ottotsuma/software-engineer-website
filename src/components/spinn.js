742d5c5e825c7d8e000e3527533afe70220524bc
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2020-10-07 18:53:30 +0900

    project 2

--- src/components/spinn.js
+++ src/components/spinn.js
@@ -5,6 +5,11 @@ import Joker from "./../assets/logo.png";
 import Secret from "./../assets/Group1.png";
 import Food from "./../assets/foodlogo.png";
 import Monster from "./../assets/monster.jpg";
+import Blog from "./../assets/blog.png";
+import Bug from "./../assets/bug.jpg";
+import Dob from "./../assets/dob.png";
+import Todo from "./../assets/todo.jpg";
+import Weather from "./../assets/weather.png";
 
 const color1 = "#202040"  
 const color2 = "#202060"
@@ -71,6 +76,54 @@ export default () => {
           </p>{" "}
           <img className="Screen-Shot" src={Joker} alt="Screen-Shot" />
           <p>Click <a href = "https://cc13-rakutenapi-staging.herokuapp.com/home" target = "_blank">here</a> to be taken to the app.</p>
+        </div>
+        <div style={{ height: 500, background: color3 }}>
+          <h4>DOB-Finder (PWA)</h4> <p>2020 April Web 1 day</p>{" "}
+          <p>
+            This application was made for Techtalk night. The application was made in to show off how easy it is to make any application a PWA and how it could be helpful for your bussiness. 
+          </p>{" "}
+          <img className="Screen-Shot" src={Dob} alt="Screen-Shot" />
+          <p>Click <a href = "https://ottotsuma.github.io/DOB-finder/html2.html" target = "_blank">here</a> to be taken to the app.</p>
+        </div>
+        <div style={{ height: 500, background: color3 }}>
+          <h4>Weather</h4> <p>2020 March Web 1 day</p>{" "}
+          <p>
+            This application was made for my personal desk top, to show me the weather when I wake up in the morning, it was made using API's and the location from your browser.  
+          </p>{" "}
+          <img className="Screen-Shot" src={Weather} alt="Screen-Shot" />
+          <p>Click <a href = "https://ottotsuma.github.io/Weather/weather.html" target = "_blank">here</a> to be taken to the app.</p>
+        </div>
+        <div style={{ height: 500, background: color3 }}>
+          <h4>Bug Tracker</h4> <p>2020 February Web 1 day</p>{" "}
+          <p>
+            This application was made to track bugs on the small projects at Digital Hearts as the client computers cannot be connected to the internet.  
+          </p>{" "}
+          <img className="Screen-Shot" src={Bug} alt="Screen-Shot" />
+          <p>Click <a href = "https://ottotsuma.github.io/BugTracker/BugTracker.html" target = "_blank">here</a> to be taken to the app.</p>
+        </div>
+        <div style={{ height: 500, background: color3 }}>
+          <h4>To Do</h4> <p>2020 March Web 1 day</p>{" "}
+          <p>
+            A small to do app made for Code Chrysalis during the pre-course.  
+          </p>{" "}
+          <img className="Screen-Shot" src={Todo} alt="Screen-Shot" />
+          <p>Click <a href = "https://ottotsuma.github.io/todo.html" target = "_blank">here</a> to be taken to the app.</p>
+        </div>
+        <div style={{ height: 500, background: color3 }}>
+          <h4>Blog</h4> <p>2019 June Web Ongoing</p>{" "}
+          <p>
+            A blog website for pratice. 
+          </p>{" "}
+          <img className="Screen-Shot" src={Blog} alt="Screen-Shot" />
+          <p>Click <a href = "https://ottotsuma.github.io/Home.html" target = "_blank">here</a> to be taken to the app.</p>
+        </div>
+        <div style={{ height: 500, background: color3 }}>
+          <h4>Game Calc</h4> <p>2019 June Web 1 day</p>{" "}
+          <p>
+            A calculator for a game. 
+          </p>{" "}
+          <img className="Screen-Shot" src={Blog} alt="Screen-Shot" />
+          <p>Click <a href = "https://ottotsuma.github.io/calchtml.html" target = "_blank">here</a> to be taken to the app.</p>
         </div>
       </ItemsCarousel>
     </div>
