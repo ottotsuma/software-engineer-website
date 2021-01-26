767d84732839b76033dbe5b3967e93fc531cffa8
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-01-26 22:23:36 +0900

    Update Ch10-Rest.js

--- src/components/Monad/Ch10-Rest.js
+++ src/components/Monad/Ch10-Rest.js
@@ -8,7 +8,10 @@ function intro(props) {
           <h4>Rest period</h4>
           {/* <!-- cross dressing wizard --> */}
           {/* Dueling and Combat. */}
-          <p></p>
+          {/* Monster is about subjugating monsters, command taking control of a small force, combat, dueling */}
+          <p>I woke early and ran down to the area, the training stones where not as busy as yesterday but I am sure they will be later. For now only a few students milled about. I went straight over to the combat stone. I ran into a problem right away, the combat stone was for groups. It was a way to fight against other groups practice your team work.</p>
+
+          <p>Mmm no worries, this might be useful in the future but right now I need to train myself. The only major training area left was the duels. This area was still busy unfortunately. </p>
           {/* <!-- Tisiel's door, left a note for you.  --> */}
         </div>
       </div>
