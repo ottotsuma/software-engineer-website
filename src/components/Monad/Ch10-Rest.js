0965202c38c77b7fab671b71d483f7189b15036c
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-01-28 19:55:38 +0900

    story

--- src/components/Monad/Ch10-Rest.js
+++ src/components/Monad/Ch10-Rest.js
@@ -23,11 +23,87 @@ function intro(props) {
             need to train myself. The only major training area left was the
             duels. This area was still busy unfortunately.
           </p>
-          <p>On the way over I saw Vasso from the lightning class. I did not expect him to be up so early since he was nobility I guess I judged him wrong. He was surrounded by boys wanting to get close to him and girls even closer. When I got to the stone it was displaying both first and second year rankings separately like the other training areas. Right at the top of the first year rankings was the name Vasso Elzach. I guess I really miss judged him.</p>
+          <p>
+            On the way over I saw Vasso from the lightning class. I did not
+            expect him to be up so early since he was nobility I guess I judged
+            him wrong. He was surrounded by boys wanting to get close to him and
+            girls even closer. When I got to the stone it was displaying both
+            first and second year rankings separately like the other training
+            areas. Right at the top of the first year rankings was the name
+            Vasso Elzach. I guess I really miss judged him.
+          </p>
 
-          <p>I guy behind me saw me looking at the rankings and leaned over to talk to me. Thinking about it, no matter which training area I went to someone was always way to nosey.</p>
+          <p>
+            I touched the stone and went inside. Inside the space was white, I
+            could move about as normal but nothing else was around. Soon a deep
+            voice came from everywhere and nowhere. "Match found." Suddenly I
+            was dropped into some sand. The sun was bright to my right and
+            across from me was another student. "Random arena: Desert. Match
+            starts in 3." Looks like it will be different each time. "2." I
+            pulled my warhammer out. "1." Cloaked myself in lightning. "Start!"
+            I ran to my opponent.
+          </p>
 
-          <p></p>
+          <p>
+            My speed started a little slow because of the sand, so I used
+            'Charged Charge' to make up some of the distance. My opponent became
+            clearer, it was hard to tell from a distance because we all have the
+            same robes but it was a blonde girl. The sand whipped up and flew
+            into my face, the wind slowed my progress and blinded me. The girl
+            must of have the wind element I thought as the sand battered my face
+            and eyes. While I was being hindered she gathered mana in her hands
+            and shot a wind cutter towards me.
+          </p>
+
+          <p>
+            It slammed into my arm since I could not see it coming easily, my HP
+            had dropped by almost 50%. None of my new spells would help me out
+            in this situation, I shot a lighting ball at her but she easily
+            avoided it. When I had got closer she just ran away, using the wind
+            to help slow me down. The match ended with my loss.
+          </p>
+
+          <p>
+            I was not ejected like the other training areas, but given the
+            choice to find a new match. I decided to try again, I guess a wind
+            user a in a desert was a bad match up for me. This time I landed in
+            a small pool of water, about 5cm deep. I stood up and saw it was
+            nothing but really shallow water all around. My opponent was not to
+            happy about it either and used his fire magic to dry himself.
+          </p>
+
+          <p>
+            Fire and Water? I should be at an advantage here with lightning. As
+            soon as the match started I used chain-lighting on the water. The
+            lightning snaked over but half way across he just evaporated the
+            water in that spot with a fireball. The water became steam and the
+            lightning fizzed out in the air.
+          </p>
+
+          <p>
+            His magic must be stronger then mine to counter it like that I
+            thought. I ran though the water letting a lighting ball fly from my
+            hands. He left loose a fireball at my lightning ball, mine collapsed
+            on impact, but his flew past me. When I had got in close he thew up
+            a wall of fire between us, I jumped into it and slammed my warhammer
+            down into his arms coving his face.
+          </p>
+
+          <p>
+            I am sure I did lot's of damage to him, but I took damage from the
+            wall of fire, and my robe was still on fire. I used 'Charged Charge'
+            into him as he point blank released another fire ball into my face.
+            I won the match with almost zero health left. I was starting to
+            think my strength would be useful later on, but I need to be faster
+            or have higher level magic to help me close the distance, right now
+            I was just being used as target practice.
+          </p>
+
+          <p>I wanted to do one more match, honestly I liked doing duels. However I decided against it. In the end I would not net any rewards, I was not good enough and the thing I wanted to do most was kill monsters. I had no interest in joining the army and fighting the kingdom in the west every 5 years, the republic in the north now the rebellion had started. I would much rather head back to Savigal and fight monsters until I am strong enough to travel out of Alden.</p>
+
+          {/* Something */}
+
+          <p>As soon as I was back in my room I saw a note on the floor.</p>
           {/* <!-- Tisiel's door, left a note for you.  --> */}
           <div className="Note">
             <h4 id="Fancy">To Otto,</h4>
