15b2764785fa06bed9354418ec997dc615e76579
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-01-28 11:46:56 +0900

    ??

--- src/components/Monad/Ch10-Rest.js
+++ src/components/Monad/Ch10-Rest.js
@@ -9,16 +9,30 @@ function intro(props) {
           {/* <!-- cross dressing wizard --> */}
           {/* Dueling and Combat. */}
           {/* Monster is about subjugating monsters, command taking control of a small force, combat, dueling */}
-          <p>I woke early and ran down to the area, the training stones where not as busy as yesterday but I am sure they will be later. For now only a few students milled about. I went straight over to the combat stone. I ran into a problem right away, the combat stone was for groups. It was a way to fight against other groups practice your team work.</p>
+          <p>
+            I woke early and ran down to the area, the training stones where not
+            as busy as yesterday but I am sure they will be later. For now only
+            a few students milled about. I went straight over to the combat
+            stone. I ran into a problem right away, the combat stone was for
+            groups. It was a way to fight against other groups practice your
+            team work.
+          </p>
 
-          <p>Mmm no worries, this might be useful in the future but right now I need to train myself. The only major training area left was the duels. This area was still busy unfortunately. </p>
+          <p>
+            Mmm no worries, this might be useful in the future but right now I
+            need to train myself. The only major training area left was the
+            duels. This area was still busy unfortunately.
+          </p>
+          <p>On the way over I saw Vasso from the lightning class. I did not expect him to be up so early since he was nobility I guess I judged him wrong. He was surrounded by boys wanting to get close to him and girls even closer. When I got to the stone it was displaying both first and second year rankings separately like the other training areas. Right at the top of the first year rankings was the name Vasso Elzach. I guess I really miss judged him.</p>
+
+          <p>I guy behind me saw me looking at the rankings and leaned over to talk to me. Thinking about it, no matter which training area I went to someone was always way to nosey.</p>
+
+          <p></p>
           {/* <!-- Tisiel's door, left a note for you.  --> */}
           <div className="Note">
-            <h4 id="Fancy">
-              To Otto,
-            </h4>
+            <h4 id="Fancy">To Otto,</h4>
             <p id="Fancy">
-                Meet me in my room, I made a break though in the language.
+              Meet me in my room, I made a break though in the language.
             </p>
           </div>
         </div>
