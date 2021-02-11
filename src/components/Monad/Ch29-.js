9e8edebe24463d8566d75a8e8ed9ebd8e35e1e06
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-02-11 14:28:46 +0900

    Update Ch29-.js

--- src/components/Monad/Ch29-.js
+++ src/components/Monad/Ch29-.js
@@ -10,8 +10,9 @@ function intro(props) {
             more points to spend then before. I could make up for the difference
             with items or titles. Right now I am missing even a weapon.
           </p>
-          <p class="equipment">Equipment:
-          {/* https://www.sacred-texts.com/cfu/mlc/mlc10.htm - obscure fire god from china */}
+          <p class="equipment">
+            Equipment:
+            {/* https://www.sacred-texts.com/cfu/mlc/mlc10.htm - obscure fire god from china */}
             <li className="Item">
               Head: <li className="Unique"> Heavenly White Stag Mask:</li>{" "}
               Magic: +1, Disease resistance + 20%, Mana regeneration + 10%,
@@ -44,6 +45,63 @@ function intro(props) {
             there had been nothing else useful. I would need boots & gloves
             also. If I went into a fight with normal boots they would just melt.
           </p>
+          <p>
+            With these spells though, I guess I should go show Ralsan the
+            farming spells and see if they are useful. I walked back though the
+            fields scaring off the crows more then anything else. They were
+            smart and knew to avoid me by a large margin now. I arrived back at
+            the warehouse to see Ralsan having an energetic conversation with a
+            woman in the distance, his arms flying all over the place as he
+            spoke. When I got close enough the conversation seemed to of ended
+            and Ralsan spotted me, he walked over waving to me. "Hey, is
+            everything okay? You came back early today." He said with a big
+            smile on his face.
+          </p>
+          <p>
+            "Yeah I am great, I just came back to show you the farming spells I
+            unlocked they seemed pretty useful but I wanted you to be the judge
+            of that." I said then added "Are you alright? I did not hear
+            anything but you seemed to be having a high energy talk with her."
+          </p>
+          <p>
+            "Ah sorry you had to see that, the academy is cutting our funding
+            again. They want to spend more of the resources and students to
+            explore tombs. Don't worry they won't ask you to go back into the
+            tombs, just the other mages who joined the club." He said a little
+            frustrated, it cannot of been easy for him to get mages to join this
+            club in the first place. "So show me these new spells you told me
+            about, we have had lightning mages join the club in the past I was
+            told but I have not seen any lightning spells used for farming
+            myself." That made me worry a little inside.
+          </p>
+          <p>
+            We walked off into a nearby patch of dark earth. It was only the
+            length and width of Ralsan and myself laying head to toe. "Hey
+            Ralsan, erm maybe just keep these spells between us if you can? I
+            don't want anyone else finding out..." I was hoping he would
+            understand.
+          </p>
+          <p>
+            He turned to me and with his usual smile "Sure thing, after all you
+            still want to join the military right? It's okay to keep them at
+            level one, this is just a club after all." That put my heart at
+            rest, he misunderstood but it would work all the same. "Thank you."
+            I replied.
+          </p>
+          <p>"So show me what you are keeping so secret." He said with a wink. I held out a seed I had taken from the field and cast 'Lightning Germination' on it while it was in my hand. The seed split and a shoot popped out. I stood there hoping he would know more about this then me.</p>
+          <p>"Did you just germinate the seed instantly? What the hell man!?" He had lost his usual smile for an open jaw.</p>
+          <p>"What? Is this unusual?" I asked panicking inside again.</p>
+          <p>"Yeah, a little." He picked up the seed from my hand. "Fire mages usually do this kind of work, it takes them a few hours which is much faster then the 3 to 5 days normally." He said looking closely at the seed, pulling out a kind of one eyed glass.</p>
+          <p>"A few hours? For one seed!?" I guess 'Lightning Germination' refereed to the speed as well as the element.</p>
+          <p>"No, hundreds usually they do them in batches. The quality is okay considering the speed though. How many can you do at once? What is the description of the spell? Whats the efficiency or fail rate?" He asked putting on a kind of business face.</p>
+          <p>I saw no reason to hide it from him so I shared the description. "Lightning germination: Stimulate a target into germinating. I tried before but it seems to be one seed at once..."</p>
+          <p>His eye brow just twitched. "I guess if you could do that to hundreds of seeds at once we would never use fire mages again." He said a little broken. "Still, no fail rate? That's all it says? 'Target' not seed? Sure is a strange one." He said his face becoming puzzled.</p>
+          <p>"Yeah I have two more spells also if this one is not useful..." I said worried that just one seed at once was not impressive.</p>
+          <p>"Oh you do? Show me them also. The spell is great just.. we can't have you doing that to hundreds of seeds, the mana consumption would be one thing but the boredom another" He laughed. "It's more like this skill was designed to work on hard to germinate seeds. Like seeds that normally fail." He said.</p>
+          <p>"Ah I don't know anything about this. Let me show you the other skills first." I cast 'Lightning Growth' on the seed and it grew on the palm of my hand. I shared the description also and let him take a look at the plant again.</p>
+          <p>"Just like that? No soil, water, food. And it's fully grown." He just shock his head as he inspected the plant.</p>
+          <p>"Last one." I said as I cast 'Lightning Harvest' and the plant fell apart as he was looking at it. I pulled out the plant from my inventory. "What's left in my palm is leftovers I guess, and this is the useful part of the plant." I said handed them both to him.</p>
+          <p>He looked at both parts for awhile, inspecting them with his little eye glass before saying. ""</p>
         </div>
       </div>
     </div>
