bf7102caea0d8524c5ef4ea33a7e4ef32f31e494
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-02-21 21:43:57 +0900

    Update Ch31-Flowers.js

--- src/components/Monad/Ch31-Flowers.js
+++ src/components/Monad/Ch31-Flowers.js
@@ -4,7 +4,37 @@ function intro(props) {
     <div className="Monad" id="3">
       <div className="Frame">
         <div className="Content">
-            <h4></h4>
+          <h4>Flowers</h4>
+          <p>The next morning I found a note had been passed under my door.</p>
+          <p id="Fancy">Dear Otto,</p>{" "}
+          <p id="Fancy">
+            Studying all the time is not good for you, let's go have some fun
+            tonight! I'll wait for you in the garden of the big house. They will
+            let you in, everyone there knows you by now.
+          </p>
+          <p>
+            The note had no name but it was clear who it was. I guess it was
+            true the people around here did recognize me now. It was not like
+            some of the other students in the academy though. The people here
+            were kind to me. Maybe it was because I was the only Alden living
+            here I did stand out a little bit. I guess I should go to class,
+            then tell Ralsan at the farm I will be going back early. Even if she
+            said to meet tonight I want to get ready, and maybe because I wanted
+            to skip any chance of drawing more attention to myself. Ralsan said
+            he would keep quiet but things got out of control really fast.
+          </p>
+          <p>
+            I washed my face and put on my school robe. The house I had moved
+            into was really big, even my room was much bigger. The whole place
+            smelt of wood all the time though, which was nice but meant I could
+            not really use magic inside the house. The only other person living
+            in the house was a kind middle aged man who made me breakfast
+            everyday. He woke up early every day because he worked at the
+            princesses house like Tsuma did. I said he did not have to make me
+            breakfast but he always did and left it behind for me with a note
+            saying it was for me.
+          </p>
+          <p></p>
           {/* Tulip seeds to impress Tsuma. */}
           {/* Survival game, eat and hide */}
         </div>
