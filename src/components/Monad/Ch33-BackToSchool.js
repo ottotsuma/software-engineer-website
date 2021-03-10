0a0856e958e45a18fe4b92dc98d13afa286e146b
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-03-10 13:21:07 +0900

    App ' instead of ' '

--- src/components/Monad/Ch33-BackToSchool.js
+++ src/components/Monad/Ch33-BackToSchool.js
@@ -15,6 +15,49 @@ function intro(props) {
             defend myself. It might not of been all of them but they seemed
             guilty by association in my mind.
           </p>
+          <p>
+            The weird thing was no one really said anything, the class room was
+            silent. I walked to an empty area at one of the desks at the front
+            and sat down. The students closest to me moved away as much as they
+            could. So petty.
+          </p>
+          <p>
+            Lubin walked into the class a little late as always and the class
+            regained some of it's normal life. "Nice to see you joining us again
+            Otto." Lubin remarked. "As you all know today is the date to vote on
+            the class champion for the battle royal coming up. The event I have
+            explained in depth last week. Otto you missed that part but I am
+            told you are up to speed right?" Lubin added at the end.
+          </p>
+          <p>
+            "Yeah the Lakian's were kind enough to explain the details to me." I
+            replied.
+          </p>
+          <p>
+            "Good, right then. Everyone can cast their vote by simple mana
+            transfer. If you cannot interact with the academy system after all
+            of that practice last week you do not get to vote. Ah Otto, you were
+            not here. Erm. Give it a go anyway." He added on the end.
+          </p>
+          <p>
+            The class sniggered at my predicament. They closed their eyes and
+            everyone seem to go into a kind of trans. Unmoving and just their
+            eye lids flickering. Lubin nudged me. "Just reach out with your
+            mana, the school is trying a new method to connect an academy wide
+            system to every student by letting them access it though their mana.
+            There have been a lot of problems so if you cannot connect don't
+            worry about it."
+          </p>
+          <p>
+            I sat and closed my eyes like the others and reached out with my
+            mana trying to move my mind around the room. Eventually I bumped
+            into others until I found what they were all reaching out to, a
+            thread of mana coming from somewhere inside the academy. I latched
+            onto it. They must put these in every class room, my sense stat is
+            rock bottom so the other students must be also.
+          </p>
+          <p>I could see though the mana, windows of writing. Forum, Shops, Voting and Reports. It seemed simple enough, like the academy was trying to modernize and make everything more efficient though the use of mana, it was really intuitive, they must of spent a lot of time creating this.</p>
+          <p>I moved into voting and saw classes by their numbers. </p>
         </div>
       </div>
     </div>
