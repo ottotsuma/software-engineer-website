42e23ee7c87c1597e19ae423c0fac9ad401b37e9
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-03-10 11:48:02 +0900

    Update Ch33-BackToSchool.js

--- src/components/Monad/Ch33-BackToSchool.js
+++ src/components/Monad/Ch33-BackToSchool.js
@@ -1,12 +1,20 @@
-
 import React from "react";
 function intro(props) {
   return (
     <div className="Monad" id="3">
       <div className="Frame">
         <div className="Content">
-            <h3>Back to school</h3>
-            <p>The next day I walked into the usual morning class room to be greeted by the most unwelcome faces I have met in my short stay on this planet. Both the boys and the girls looked at me like I was </p>
+          <h3>Back to school</h3>
+          <p>
+            The next day I walked into the usual morning class room to be
+            greeted by the most unwelcome faces I have met in my short stay on
+            this planet. Both the boys and the girls looked at me like I was
+            some kind of traitor. The last time I saw them all it was they that
+            betrayed me. They had planned to attack me while the teacher was not
+            looking and not stop until I was badly hurt or dead all I did was
+            defend myself. It might not of been all of them but they seemed
+            guilty by association in my mind.
+          </p>
         </div>
       </div>
     </div>
