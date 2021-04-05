5baf40fa5218c0af27236d4a687661256134654a
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-04-05 21:43:45 +0900

    Update Ch2-Travel.js

--- src/components/Monad/Ch2-Travel.js
+++ src/components/Monad/Ch2-Travel.js
@@ -300,8 +300,10 @@ function intro(props) {
             The river was just past were we found the student, the water was murky like it was full of old milk mixed in with the water. The other students would go around the same river further up stream. That would take them past the source of the river and around so they would not have to cross the water itself. Since we had run downstream we would have to cross the river. "Leon, is the water always this color?" I asked looking into it.
           </p>
           <p>"No, not further down, just up here. People say it is because of the chalk in the ground that causes it while others say it is the monsters living in the river this far up. Either way don't touch it." He said with a serious face.</p>
-          <p>We walked along the river bank until we found some dead trees and pushed them across the river to make a bridge. Leon went first since it was his idea to come this way and walked across the tree while I held it still on my side. I say held it still, the tree was really thick and long so it was not like I helped a lot.</p>
-          <p></p>
+          <p>We walked along the river bank until we found some dead trees and pushed them across the river to make a bridge. Leon went first since it was his idea to come this way and walked across the tree while I held it still on my side. I say held it still, the tree was really thick and long, so it was not like I helped a lot.</p>
+          <p>Once Leon got across he held the tree on the opposite side and I walked across the tree. I tried to keep low and move quickly. When I was almost one third of the way across my leg sunk into the tree and I lost my balance and landed on the tree hugging it. Once I realised what happened I laughed a little.</p>
+          <p>The old tree was a little hollow I guess. I tried to pull myself back up but my foot was wedged into the hole. Then a pair of vertical oval shaped eyes appeared in the river looking up at me. My face felt cold and my limbs numb.</p>
+          <p>I had no idea what that thing was looking at me. It sure was no fish since the eyes were next to each other like a human. I dragged my foot as hard as I could against the tree bark trying to break free.</p>
           {/* Keep writing until arriving near the city */}
         </div>
       </div>
