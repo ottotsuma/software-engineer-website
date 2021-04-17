9e51ea0f5604b24b6dc153dac6a1a18bec795776
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-04-17 10:23:04 +0900

    Update Ch34-Duel.js

--- src/components/Monad/Ch34-Duel.js
+++ src/components/Monad/Ch34-Duel.js
@@ -16,6 +16,8 @@ export default intro;
 // Merging the stats into a few, Vitality, magic, strength, sense, charm
 // No classes? Just race and element?
 
+// Transferring class of the same rank and type lose 0~9[dice rolling thing] levels.
+
 function placeholder() {
   return (
     <div>
@@ -23,11 +25,16 @@ function placeholder() {
       <p>A group of sea travelers with different races. Their classes will be advanced classes because they have all reached level 100 at some point. The miasma effect will reduce their capabilities by 90% and be removed as they complete more quests on the DLC island.</p>
       <h2>List of the group</h2>
       <ul>
-        <li>Example: Name, race, class(), level, equipment, backstory(Main island, other dlc before? ), clan/guild, reason for coming.</li>
-        <li>Boy: Name, race, class(Rogue[]), level, equipment, (From the main island, recently started playing, no dlc), none, saw the adverts for coming and wanted to try.</li>
-        <li>Girl: Name, race, class(Tank[]), level, equipment, (Pro-gamer, guild leader, small dlc before), clan/guild, get in before the other guilds.</li>
-        <li>Boy: Name, Human, class(Rogue[]), level, equipment, (Member of the same guild, small dlc, company man), clan/guild, accompany the same guild.</li>
+        <li>Example: Name, race, class(), level, equipment, backstory(Main island, other dlc before? ), clan/guild, reason for coming. PET(level)</li>
+
+{/* Human into something? */}
+        <li>Boy: Name, Dark Elf?, class(Rogue[]), 37, [Normal/Rare, mostly from the main quests], (From the main island, recently started playing, no dlc), none, saw the adverts for coming and wanted to try. None(0)</li>
+        <li>Girl: Sarah, Vampire, Use to be a human paladin, class(Tank[Guardian]) Wants to change to blood warrior, 100, [mixed normal, rare epic with one unique], (Pro-gamer, guild leader, small dlc before), clan/guild, get in before the other guilds. PET(60)</li>
+        {/*looks; file:///C:/Users/snows/Documents/GitHub/ottotsuma.github.io/guard100.html */}
+        <li>Boy: Josh, Human, class(Rogue-subtype[Assassin]) Recently became assassin, 92, [file:///C:/Users/snows/Documents/GitHub/ottotsuma.github.io/Assassin.html , but not unique items], (Member of the same guild, small dlc, company man), clan/guild, accompany the same guild. RAT(100)</li>
       </ul>
     </div>
   );
 }
+
+// Team bonuses for each of them?
