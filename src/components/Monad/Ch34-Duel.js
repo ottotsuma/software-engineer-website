2b965180fcec26ec8c57f19a72038c9fc5ee9025
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-04-18 14:17:54 +0900

    Update Ch34-Duel.js

--- src/components/Monad/Ch34-Duel.js
+++ src/components/Monad/Ch34-Duel.js
@@ -22,17 +22,47 @@ function placeholder() {
   return (
     <div>
       <h1>Sea people</h1>
-      <p>A group of sea travelers with different races. Their classes will be advanced classes because they have all reached level 100 at some point. The miasma effect will reduce their capabilities by 90% and be removed as they complete more quests on the DLC island.</p>
+      <p>
+        A group of sea travelers with different races. Their classes will be
+        advanced classes because they have all reached level 100 at some point.
+        The miasma effect will reduce their capabilities by 90% and be removed
+        as they complete more quests on the DLC island.
+      </p>
       <h2>List of the group</h2>
       <ul>
-        <li>Example: Name, race, class(), level, equipment, backstory(Main island, other dlc before? ), clan/guild, reason for coming. PET(level)</li>
+        <li>
+          Example: Name, race, class(), level, equipment, backstory(Main island,
+          other dlc before? ), clan/guild, reason for coming. PET(level)
+        </li>
 
-{/* Human into something? */}
-        <li>Boy: Name, Dark Elf?, class(Rogue[]), 37, [Normal/Rare, mostly from the main quests], (From the main island, recently started playing, no dlc), none, saw the adverts for coming and wanted to try. None(0)</li>
-        <li>Girl: Sarah, Vampire, Use to be a human paladin, class(Tank[Guardian]) Wants to change to blood warrior, 100, [mixed normal, rare epic with one unique], (Pro-gamer, guild leader, small dlc before), clan/guild, get in before the other guilds. PET(60)</li>
+        {/* Human into something? */}
+        <li>
+          Boy: Name, Dark Elf?, class(Rogue[]), 37, [Normal/Rare, mostly from
+          the main quests], (From the main island, recently started playing, no
+          dlc), none, saw the adverts for coming and wanted to try. None(0)
+        </li>
+        <li>
+          Girl: Sarah, Vampire, Use to be a human paladin, class(Tank[Guardian])
+          Wants to change to blood warrior, 100, [mixed normal, rare epic with
+          one unique], (Pro-gamer, guild leader, small dlc before), clan/guild,
+          get in before the other guilds. PET(60)
+        </li>
         {/*looks; file:///C:/Users/snows/Documents/GitHub/ottotsuma.github.io/guard100.html */}
-        <li>Boy: Josh, Human, class(Rogue-subtype[Assassin]) Recently became assassin, 92, [file:///C:/Users/snows/Documents/GitHub/ottotsuma.github.io/Assassin.html , but not unique items], (Member of the same guild, small dlc, company man), clan/guild, accompany the same guild. RAT(100)</li>
+        <li>
+          Boy: Josh, Human, class(Rogue-subtype[Assassin]) Recently became
+          assassin, 92,
+          [file:///C:/Users/snows/Documents/GitHub/ottotsuma.github.io/Assassin.html
+          , but not unique items], (Member of the same guild, small dlc, company
+          man), clan/guild, accompany the same guild. RAT(100)
+        </li>
       </ul>
+      <p>"We'll be there soon." Josh said from behind me.</p>
+      <p>"Awesome, I don't want to spend another minute on board this ship." I said holding onto the side of the ship looking out into the big blue ocean.</p>
+      <p>"Who knew you could get sick inside of a game? I feel bad for you man." Josh said patting my back. That didn't help so I hugged the side of the ship tighter.</p>
+      <p>"And I said if you call it a game you won't be joining me on this trip." Sarah said walking out from the captains cabin. Her skin was pale white, jade like and smooth. Her white skin somehow made her bright orange hair stand out more. In contrast her armor was mostly savoy blue armor trimmed in gold, her cloak billowed out behind her in the wind, white with gold trim. She looked almost holy if it was not for her large fangs.</p>
+      <p>She locked her steely blue eyes on Josh and reminded him. "We are roleplaying. So you can't call it a game, at best another world. Don't forget what happened last time.. If we do not blend in with the natives they will kill us."</p>
+      <p>Josh responded with a nod. "Yeah, I will remember when we land. I promise." He smiled and patted me on the back again.</p>
+      <p></p>
     </div>
   );
 }
