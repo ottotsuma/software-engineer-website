75320a032c6218add4bdd34d1a38066534004f1e
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-04-23 16:44:08 +0900

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,19 +3,21 @@ const path = require('path');
   const app = express();
   const isDev = process.env.NODE_ENV !== 'production';
   const PORT = process.env.PORT || 5000;
+  const CacheTime = 86400 // 1 day
+  // ('Cache-Control', 'public, max-age=2628000') // 31536000 1 year // 2628000 almost a month
 
   // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../build')));
+  app.use(express.static(path.resolve(__dirname, '../build'),{
+    maxAge: CacheTime
+  }));
 
   // Answer API requests.
   app.get('/api', function (req, res) {
-    res.set('Content-Type', 'application/json');
     res.send('{"message":"Hello from the custom server!"}');
   });
 
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', function(request, response) {
-    response.set('Cache-Control', 'public, max-age=2628000') // 31536000 1 year // 2628000 almost a month
     response.sendFile(path.resolve(__dirname, '../build', 'index.html'));
   });
 
