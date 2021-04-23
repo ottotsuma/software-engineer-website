9f002ad9edf1b63e0c88f4643dee53c817958ecc
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-04-23 23:00:04 +0900

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -10,20 +10,34 @@ import styled from 'styled-components'
 function Nav() {
   return (
     <NavigationBar>
-        <Link className="Nav-Link" to="/">Home</Link>
-        <Link className="Home-Button" to="/">Home</Link>
-        {/* <Link className="Nav-Link" to="/projects">Projects</Link> */}
-        {/* <Link className="Nav-Link" to="/timeline">Experience</Link> */}
-        {/* <Link className="Nav-Link" to="/tech">Technical Skills</Link> */}
-        <a className="Nav-Link" href = 'https://github.com/ottotsuma' target = "_blank">GitHub</a>
-        {/* <Link className="Nav-Link" to="/Blackjack">Blackjack</Link> */}
-      <Link className="Home-Button" to="/Monad/1">Monad</Link>
-      <Link className="Nav-Link" to="/Monad/1">Monad</Link>
+        <LinkStyled className="Nav-LinkStyled" to="/">Home</LinkStyled>
+        <LinkStyled className="Home-Button" to="/">Home</LinkStyled>
+        {/* <LinkStyled className="Nav-LinkStyled" to="/projects">Projects</LinkStyled> */}
+        {/* <LinkStyled className="Nav-LinkStyled" to="/timeline">Experience</LinkStyled> */}
+        {/* <LinkStyled className="Nav-LinkStyled" to="/tech">Technical Skills</LinkStyled> */}
+        <AStyled className="Nav-LinkStyled" href = 'https://github.com/ottotsuma' target = "_blank">GitHub</AStyled>
+        {/* <LinkStyled className="Nav-LinkStyled" to="/Blackjack">Blackjack</LinkStyled> */}
+      <LinkStyled className="Home-Button" to="/Monad/1">Monad</LinkStyled>
+      <LinkStyled className="Nav-LinkStyled" to="/Monad/1">Monad</LinkStyled>
     </NavigationBar>
   );
 }
 
 export default Nav;
+
+const AStyled = styled.a`
+color: white;
+&:hover {
+    color: orange;
+}
+`;
+
+const LinkStyled = styled(Link)`
+color: white;
+&:hover {
+    color: orange;
+}
+`;
 
 const NavigationBar = styled.div`
 min-height: 4vh;
