795d407e7a2133d7f7dc0f3f3d647d92ac7ba9d2
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-05-22 08:47:50 +0900

    Update spells.js

--- src/components/elements/spells.js
+++ src/components/elements/spells.js
@@ -12,187 +12,182 @@ import React from "react";
 const spellList = {
   "Example": {
     name: "Example",
-    description: "Example",
+    1: "Example",
   },
   "Charged Charge": {
     name: "Charged Charge",
-    description: "Stimulates your muscles in your legs with lightning shocks, to boost your speed.",
+    1: "Stimulates your muscles in your legs with lightning shocks, to boost your speed.",
   },
   "Lightning Storm": {
     name: "Lightning Storm",
-    description: "Example",
+    1: "Example",
   },
   "Wall of lightning": {
     name: "Wall of lightning",
-    description: "Example",
+    1: "Example",
   },
   "Static Field": {
     name: "Static Field",
-    description: "Example",
+    1: "Example",
   },
   "Thunderbolt": {
     name: "Thunderbolt",
-    description: "Calls a thunder bolt from above onto the target.",
+    1: "Calls a thunder bolt from above onto the target.",
   },
   "Discharge": {
     name: "Discharge",
-    description: "Example",
+    1: "Example",
   },
   "Lightning Cloak": {
     name: "Lightning Cloak",
-    description: "Covers yourself in lighting, shocking anyone who comes too close.",
+    1: "Covers yourself in lighting, shocking anyone who comes too close.",
   },
   "Lightning Rune": {
     name: "Lightning Rune",
-    description: "Injects lighting into a surface, will release when someone comes near.",
+    1: "Injects lighting into a surface, will release when someone comes near.",
   },
   "Lightning Bolt": {
     name: "Lightning Bolt",
-    description: "Shoots a bolt of lightning at a target.",
+    1: "Shoots a bolt of lightning at a target.",
   },
   "Sparks": {
     name: "Sparks",
-    description: "Example",
+    1: "Example",
   },
   "Shocking Touch": {
     name: "Shocking Touch",
-    description: "Electrocutes a target you touch [You decide the body part it comes from].",
+    1: "Electrocutes a target you touch [You decide the body part it comes from].",
   },
   Thunderstorm: {
     name: "Thunderstorm",
-    description: "Example",
+    1: "Example",
   },
   "Dazing Bolt": {
     name: "Dazing Bolt",
-    description: "Example",
+    1: "Example",
   },
   "Chain Lightning": {
     name: "Chain Lightning",
-    description: "Shoots lightning at a target, part of the lighting will be redirected to near by targets.",
+    1: "Shoots lightning at a target, part of the lighting will be redirected to near by targets.",
   },
   "Serpent Strike": {
     name: "Serpent Strike",
-    description: "Your next physical attack will be faster.",
+    1: "Your next physical attack will be faster.",
   },
   "Serpent Pattern": {
     name: "Serpent Pattern",
-    description:
+    1:
       "Your skin will adapt to the environment slightly, increased stealth.",
   },
   "Forked Tongue": {
     name: "Forked Tongue",
-    description: "Your tongue will become forked, Increased sense of smell.",
+    1: "Your tongue will become forked, Increased sense of smell.",
   },
   "Serpent-Skin": {
     name: "Serpent-Skin",
-    description:
+    1:
       "Your skin slowly becomes like a serpents. Able to turn on and off scaled armor over your skin.",
   },
   Haietlik: {
     name: "Haietlik",
-    description:
+    1:
       "Throw a snake shaped lighting bolt to hook onto a target, dealing damage in the process.",
   },
   "Kʼawiil Bolt": {
     name: "Kʼawiil Bolt",
-    description: "Releases a burst of Kʼawiil-lightning from range.",
+    1: "Releases a burst of Kʼawiil-lightning from range.",
   },
   "Kʼawiil Burst": {
     name: "Kʼawiil Burst",
-    description: "Releases a burst of Kʼawiil-lightning on contact.",
+    1: "Releases a burst of Kʼawiil-lightning on contact.",
   },
   "Lightning Plant": {
     name: "Lightning Plant",
-    description: "Turn lightning seeds into lightning plants!",
+    1: "Turn lightning seeds into lightning plants!",
   },
   "Inspect Plants": {
     name: "Inspect Plants",
-    description:
+    1:
       "Your inspection of plants will be a level up on your normal inspection ability.",
   },
   "Bountiful Harvest": {
     name: "Bountiful Harvest",
-    description:
+    1:
       "All of your harvests are increased by [x]%. [x is 10x the level of this spell.]",
   },
   "Reap what you Sow": {
     name: "Reap what you Sow",
-    description: "Reap the benefits or punishments from the seeds you sow.",
+    1: "Reap the benefits or punishments from the seeds you sow.",
   },
   Scarecrow: {
     name: "Scarecrow",
-    description: "Uses plant material to summon a sentient scarecrow.",
+    1: "Uses plant material to summon a sentient scarecrow.",
   },
   "Lightning harvest": {
     name: "Lightning harvest",
-    description:
+    1:
       "Harvest the target. [If the target is below 20% HP this attack does 200% more damage. If this target is a 'plant' type this attack does 500% more damage. If this attack kills the target you have a chance to harvest something.]",
   },
   "Lightning growth": {
     name: "Lightning growth",
-    description: "Force the target to grow.",
+    1: "Force the target to grow.",
   },
   "Lightning germination": {
     name: "Lightning germination",
-    description: "Stimulate a target into germinating",
+    1: "Stimulate a target into germinating",
   },
   "Seeds to Soldiers": {
     name: "Seeds to Soldiers",
-    description:
+    1:
       "Take [x] seed(s), use energy to give life to [x] soldier(s). [x is equal to spell level.]",
   },
   "Lightning seeds": {
     name: "Lightning seeds",
-    description:
+    1:
       "Shoots a group of [x] seed(s) in a spread from the user. The seeds attach to opponent(s) and cause lightning damage over time. A part of the damage is used to restore your as mana. [x is equal to spell level.]",
   },
   "Mana Ball": {
     name: "Mana Ball",
-    description: "Gathers mana into a ball. Gives off a small amount of light.",
+    1: "Gathers mana into a ball. Gives off a small amount of light.",
   },
   Tether: {
     name: "Tether",
-    description: "Gathers mana into a string shape.",
+    1: "Gathers mana into a string shape.",
   },
   "Magic Resistance": {
     name: "Magic Resistance",
-    description: "Passive: Resists mana foreign to the body, on surface level.",
+    1: "Passive: Resists mana foreign to the body, on surface level.",
   },
   "Magic Up": {
     name: "Magic Up",
-    description: "Increases the intensity of mana in the body.",
+    1: "Increases the intensity of mana in the body.",
   },
   Strike: {
     name: "Strike",
-    description:
+    1:
       "Calls mana in the environment to gather and strike down from above.",
   },
   "Magic Regen": {
     name: "Magic Regen",
-    description: "Increases mana recovery speed.",
+    1: "Increases mana recovery speed.",
   },
 };
 // Add spell (level/rank). Changes description.
 function Spells({ spells }) {
-  const tempSpells = []
-  for (let index = 0; index < spells.length; index++) {
-    const element = spells[index].name;
-    tempSpells.push(element)
-  }
   const array = [];
-  for (let index = 0; index < tempSpells.length; index++) {
-    const element = spellList[tempSpells[index]];
+  for (let index = 0; index < spells.length; index++) {
+    const element = spellList[spells[index].name];
     if (element) {
       array.push(
         <SingleSpell key={index + "SingleSpell"}>
           {element.name}
-          <Span>{element["description"]}</Span>
-        </SingleSpell>
+          {element[spells[index].level] ? <Span>{element[spells[index].level]}</Span> : <Span>{spells[index].name}</Span>}
+        </SingleSpell> 
       );
     } else {
       array.push(
         <SingleSpell key={index + "SingleSpell"}>
-          {tempSpells[index]}
+          {spells[index].name}
         </SingleSpell>
       )
     }
