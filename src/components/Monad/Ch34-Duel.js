05e73efd29116bfc9a0f634d060678919c8d1c91
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-05-31 09:43:08 +0900

    Update Ch34-Duel.js

--- src/components/Monad/Ch34-Duel.js
+++ src/components/Monad/Ch34-Duel.js
@@ -1172,8 +1172,9 @@ export const SeaPeople = {
         her.
       </p>
       <p>
-        
+        As we became targets, Sarah moved her shield in the way to block them from destroying our boat. I just kept rowing as the attacks became more frequent and stronger. The closer got to the shore the more we became a target, however Sarah kept standing strong blocking all of the attacks coming our direction. We were so close to the shore by this point I could almost walk onto the beach, but decided I should just keep rowing, even if I made it onto the beach the cliffs straddled either side so I would surely die without Sarah.
       </p>
+      <p></p>
     </Content>
   ),
   7: <Content style={{ overflowWrap: "anywhere", width: "100%" }}>7</Content>,
