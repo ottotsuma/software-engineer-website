46d7c7e973e7896b9a258ea6da76b7f88b3dc159
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-06-06 18:59:08 +0900

    spells

--- src/components/elements/spells.js
+++ src/components/elements/spells.js
@@ -564,7 +564,226 @@ export const spellList = {
     passive: false,
     1: "You create illusory noises that appear to come from a direction of your choice.",
   },
+  "Beacon": {
+    name: "Beacon",
+    passive: false,
+    1: "A touched object begins emitting a psychic pulse that draws the curiosity of all monsters within 1 mile.",
+  },
+  "Beast Form": {
+    name: "Beast Form",
+    passive: false,
+    1: "You transform into a mundane animal.",
+  },
+  "Befuddle": {
+    name: "Befuddle",
+    passive: false,
+    1: "1 creature of your choice are unable to form new short-term memories for the duration of the spell.",
+  },
+  "Bird Person": {
+    name: "Bird Person",
+    passive: false,
+    1: "Your arms turn into huge wings.",
+  },
+  "Body Swap": {
+    name: "Body Swap",
+    passive: false,
+    1: "You switch bodies with a creature you touch. If one body dies, the other dies as well.",
+  },
+  "Charm": {
+    name: "Charm",
+    passive: false,
+    1: "A creature treat you like a friend.",
+  },
+  "Command": {
+    name: "Command",
+    passive: false,
+    1: "A creature obeys a single, three-word command that does not harm it.",
+  },
+  "Comprehend": {
+    name: "Comprehend",
+    passive: false,
+    1: "You become fluent in all languages.",
+  },
+  "Control Weather": {
+    name: "Control Weather",
+    passive: false,
+    1: " You may alter the type of weather at will, but you do not otherwise control it.",
+  },
+  "Counterspell": {
+    name: "Counterspell",
+    passive: false,
+    1: "Chance to cancel a targets spell.",
+  },
+  "Deafen": {
+    name: "Deafen",
+    passive: false,
+    1: "Nearby creatures of your choice are deafened.",
+  },
+  "Disassemble": {
+    name: "Disassemble",
+    passive: false,
+    1: "Any of your body parts may be detached and reattached at will, without causing pain or damage. You can still control them.",
+  },
+  "Disguise": {
+    name: "Disguise",
+    passive: false,
+    1: "You may alter the appearance of 1 characters at will as long as they remain humanoid. Attempts to impersonate other characters will seem uncanny.",
+  },
+  "Displace": {
+    name: "Displace",
+    passive: false,
+    1: "Shift an object’s apparent position by up to 10cm.",
+  },
+  "Earthquake": {
+    name: "Earthquake",
+    passive: false,
+    1: "The ground begins shaking violently. Structures may be damaged or collapse.",
+  },
 };
+
+// Elasticity: Your body can stretch up to 30’.
+
+// Elemental Wall: A wall of ice or fire Lx 30’ long appears.
+
+// Feast: A huge table appears, laden with food.
+
+// Filch: L visible items teleport to your hands.
+
+// Flesh Warp: A touched creature doubles or halves in size.
+
+// Fog Cloud: A cloud of dense fog spreads out from you.
+
+// Frenzy: L creatures erupt in a frenzy of random violence.
+
+// Gate: A portal to a random plane opens.
+
+// Gravity Shift: You can change the direction of gravity (for yourself only) once every 6 seconds.
+
+// Haste: Your movement speed is tripled.
+
+// Hear Whispers: You can hear the faintest sounds clearly.
+
+// Hover: An object hovers, frictionless, 2’ above the ground. Can hold up to 5 humanoids.
+
+// Hypnotize: A creature enters a trance and will truthfully answer L yes or no questions you ask it.
+
+// Illuminate: A floating light moves as you command it.
+
+// Increase Gravity: The gravity in a nearby area triples.
+
+// Invisible Tether: Two objects within 10’ of each other be-come linked and cannot be moved more than 10’ apart.
+
+// Ironbane: Any iron that touches you disintegrates.
+
+// Knock: All nearby locks, clasps, and buckles open.
+
+// Leap: You can jump up to 30’ at a time.
+
+// Liquid Air: The air becomes thick enough to swim in.
+
+// Magic Dampener: All nearby magical effects have their ef-fectiveness halved.
+
+// Manse: A sturdy, fully furnished cottage appears. You can permit and forbid entry to it at will. Lasts Lx12 hours.
+
+// Marble Madness: Your pockets are always full of marbles.
+
+// Masquerade: L characters’ appearances and voices become identical to yours.
+
+// Miniaturize: You and L other touched creatures are reduced to the size of a mouse.
+
+// Mirror Image: L illusory duplicates of yourself appear under your control.
+
+// Mirrorwalk: A mirror becomes a gateway to another mirror that you have looked into today.
+
+// Multiarm: You gain L extra arms until end of spell.
+
+// Objectify: You become any inanimate object between the size of a grand piano and an apple.
+
+// Ooze Form: You become a living jelly.
+
+// Pacify: L creatures have a sudden aversion to violence.
+
+// Phantom Coach: A ghostly coach appears until end of spell. It moves unnaturally fast over any terrain, even water.
+
+// Phobia: L creatures are terrified of an object of your choice.
+
+// Primeval Surge: A mundane animal grows to the size of an elephant and is enraged.
+
+// Psychometry: The referee answers L yes or no questions about a touched object.
+
+// Pull: An object of any size is pulled 30’ directly towards you with the strength of L men. Lasts for one round.
+
+// Push: An object of any size is pushed 30’ directly away from you with the strength of L men. Lasts for one round.
+
+// Raise Spirit: The spirit of a dead body manifests and may choose to answer questions.
+
+// Read Mind: You hear the surface thoughts of creatures.
+
+// Repel: L+1 objects are strongly magnetically repelled from each other if they come within 10 feet.
+
+// Scry: You can see through the eyes of a known creature.
+
+// Sculpt Elements: All inanimate material behaves like clay in your hands.
+
+// Shroud: L creatures become invisible until they move.
+
+// Shuffle: L creatures instantly switch places.
+
+// Sleep: L creatures fall into a light sleep.
+
+// Smoke Form: Your body becomes living smoke.
+
+// Snail Knight: 10 minutes after casting, a knight sitting astride a giant snail rides into view. He is able to answer most questions related to quests and chivalry, and may aid you if he finds you worthy.
+
+// Sniff: You can smell even the faintest traces of scents.
+
+// Sort: Inanimate items sort themselves according to categories you set. The categories must be visually verifiable.
+
+// Spatial Distortion: A nearby inanimate object no bigger than a bedroom shrinks to the size of an apple.
+
+// Spectacle: A clearly unreal but impressive illusion of your choice appears, under your control. It may be up to the size of a palace and has full motion and sound.
+
+// Spellseize: You may steal or erase L spells from the mind of a nearby creature.
+
+// Spider Climb: You can climb surfaces like a spider.
+
+// Summon Cube: Once per second, (6 times per round) you may summon or banish 3-foot-wide cubes of earth. New cubes must be affixed to the earth or to other cubes.
+
+// Swarm: You become a swarm of crows, rats, or piranha.
+
+// Telekinesis: You may mentally move L items.
+
+// Telepathy: L+1 creatures can hear each other’s thoughts.
+
+// Tesser: An object disappears and reappears on the ground in a visible, clear area up to Lx30’ away.
+
+// Thaumaturgic Anchor: Object becomes the target of every spell cast near it.
+
+// Thicket: A thicket of trees and dense brush up to Lx30’ wide suddenly sprouts up.
+
+// Time Jump: An object jumps Lx10 minutes into the future.
+
+// Time Rush: Time in a nearby area becomes 10 times faster.
+
+// Time Slow: Time in a nearby area slows to 10%.
+
+// Transfer Heat: You may transfer heat or cold between two inanimate objects.
+
+// True Sight: You see the true nature of your surroundings.
+
+// Upwell: A spring appears and starts gushing seawater.
+
+// Vision: You have total control over what a creature sees.
+
+// Visual Illusion: A silent, immobile, illusion of your choice appears, up to the size of a bedroom.
+
+// Ward: A silver circle 30’ across appears on the ground. Choose one thing that cannot cross it: Living creatures, dead creatures, non-human creatures, projectiles or metal.
+
+// Web: Your hands can shoot thick webbing.
+
+// Wizard Lock: A door is magically sealed.
+
+// Wizard Mark: Your finger can shoot a stream of brightly-colored paint. This paint is only visible to wizards, and can be seen at any distance, even through solid objects. Fades after L days.
 
 // https://i.4pcdn.org/tg/1498966145042.pdf
 
