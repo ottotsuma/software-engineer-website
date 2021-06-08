67370844acf74b365aa6b6b340e22ad08a564626
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-06-08 13:03:53 +0900

    Update SpellsPage.js

--- src/components/Monad/SpellsPage.js
+++ src/components/Monad/SpellsPage.js
@@ -2,19 +2,22 @@ import React, { useState } from "react";
 import { spellList } from "./../elements/spells";
 import Spells from "./../elements/spells";
 import styled from "styled-components";
+import { Content } from "./styles";
 
 export default function SpellsPage() {
-const array = Object.keys(spellList);
-const allSpellsArray = [];
-for (let index = 0; index < array.length; index++) {
-  const element = array[index];
-  allSpellsArray.push({ name: element, level: 1 });
-}
+  const array = Object.keys(spellList);
+  const allSpellsArray = [];
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    allSpellsArray.push({ name: element, level: 1 });
+  }
 
   return (
-    <Wrapper>
-      <Spells spells={allSpellsArray} type={"mage"} />
-    </Wrapper>
+    <Content>
+      <Wrapper>
+        <Spells spells={allSpellsArray} type={"mage"} />
+      </Wrapper>
+    </Content>
   );
 }
 
