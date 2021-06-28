0b70e438a0d32e497b34e2888dd078154bfe487f
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-06-28 21:45:26 +0900

    Update Vampire.js

--- src/components/Monad/Vampire.js
+++ src/components/Monad/Vampire.js
@@ -55,18 +55,3 @@ export default function Vampire() {
     </div>
   );
 }
-
-// https://www.pinterest.jp/pin/3659243432263631/
-// https://www.pinterest.jp/pin/1407443622023019/
-// https://www.pinterest.jp/pin/985231157601720/
-// https://www.pinterest.jp/pin/2251868547110588/
-// https://www.pinterest.jp/pin/633178028835316448/
-// https://www.pinterest.jp/pin/28006828922357029/
-// https://www.pinterest.jp/pin/222576406564801885/
-// https://www.pinterest.jp/pin/778067273118206650/
-// https://www.pinterest.jp/pin/3799980924655989/
-// https://www.pinterest.jp/pin/535154368226474776/
-// https://www.pinterest.jp/pin/338332990762953977/
-// https://www.pinterest.jp/pin/545639311112087999/
-// https://www.pinterest.jp/pin/430234570646376643/
-// https://www.pinterest.jp/pin/328059154104084463/
