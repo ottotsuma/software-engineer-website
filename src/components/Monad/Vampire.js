01af1bc2a34f7bd4874040878d414823b8d443db
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-08-07 15:35:25 +0900

    Update Vampire.js

--- src/components/Monad/Vampire.js
+++ src/components/Monad/Vampire.js
@@ -5,7 +5,7 @@ import Stats from "./../elements/stats";
 import Spells from "./../elements/spells";
 
 // Vampires can gain experience by trying new blood, creature ghouls and the exp gained from the ghouls fighting
-// all actions that would reveal the vampire would either cure or gain them exp or blood points to be used to improve their strength 
+// all actions that would reveal the vampire would either cure or gain them exp or blood points to be used to improve their strength
 // that way younger vampires are more likely to do these things then the older ones who don't need strength as badly
 // Combined with the starving yourself (lower hp) increases you strength
 
@@ -45,14 +45,36 @@ export default function Vampire() {
           { name: "Blood Sacrifice", level: 1 },
           { name: "Blood Wound", level: 1 },
           { name: "Blood Control", level: 1 },
-          
+          { name: "Blood Hardening", level: 1 },
+          { name: "Blood Swipe", level: 1 },
+          { name: "Blood Expand", level: 1 },
+          { name: "Blood Barrage", level: 1 },
+          { name: "Blood Bullet", level: 1 },
+          { name: "Blood Bank", level: 1 },
+          { name: "Blood Wall", level: 1 },
+          { name: "Blood Spray", level: 1 },
         ]}
         type={"mage"}
       />
+      {/* 
+      https://my-vampire-system.fandom.com/wiki/Quinn_Talen/Abilities
+      */}
       <div className="BreakPoint"></div>
       <h1>Vampire specific skills (Blood + Other)</h1>
       <Spells
         spells={[
+          { name: "Hammer Strike", level: 1 },
+          { name: "Blood Hammer", level: 1 },
+          { name: "Blood Hammer Drill", level: 1 },
+          { name: "Blood Crescent Kick", level: 1 },
+          { name: "Daze", level: 1 },
+          { name: "Phantom Punch", level: 1 },
+          { name: "Phantom Kick", level: 1 },
+          { name: "Blood Ritual", level: 1 },
+          { name: "Flash Step", level: 1 },
+          { name: "Basic Vampire Martial Arts", level: 1 },
+
+          { name: "Vampiric Influence", level: 1 },
           {
             name: "Blood Sucker",
             level: 1,
@@ -60,7 +82,6 @@ export default function Vampire() {
           { name: "Black Shroud", level: 1 },
           { name: "Vampire Eyes", level: 1 },
           { name: "Aura of Despair", level: 1 },
-
         ]}
         type={"human"}
       />
