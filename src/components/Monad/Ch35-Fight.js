48cd4c0438929f5e94452228cd0729635dc1c036
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-08-22 15:37:24 +0900

    Chu-gong

--- src/components/Monad/Ch35-Fight.js
+++ src/components/Monad/Ch35-Fight.js
@@ -4,6 +4,17 @@ import { Content } from "./styles"
 function Intro(props) {
 return (
 <Content>
+    <h4>Fight!</h4>
+    {/* Showing of the arena and the contestants inside the jungle or walking to the area? */}
+    <p></p>
+    {/* 'Fans' making bets and saying the most retarded things you can think of */}
+    {/* Some retard teacher explaining retarded rules */}
+    {/* The start of the event with people running away, retards running for the middle to battle out the 'strongest' */}
+    {/* tracking of students though the jungles */}
+    {/* Dumb 'events' */}
+    {/* More dumb student banter */}
+    {/* Cornering students and beating them to death */}
+    {/* running away */}
 </Content>
 );
 }
--- src/components/Monad/Ch36-Farm.js
+++ src/components/Monad/Ch36-Farm.js
@@ -4,8 +4,18 @@ import { Content } from "./styles"
 function Intro(props) {
 return (
 <Content>
+    <h4>Farm!?</h4>
+    {/* Found Otto's 'Farm' */}
+    {/* Otto Vs Morons */}
+    {/* Living in the farm */}
+    {/* Last of the morons */}
+    {/* it's love! ... No it's not */}
+    {/* off to fight in the war */}
+    <p></p>
 </Content>
 );
 }
 export default Intro;
 // The event ends with the organizers forcing an end since Kaja and Otto refuse to fight and wont die of starvation. They claim it shows how great the students of Alden are.
+
+// 
