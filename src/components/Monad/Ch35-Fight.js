d4319c9808343abaef108225a5cddc6b4e6d4afa
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-08-26 18:22:13 +0900

    Update Ch35-Fight.js

--- src/components/Monad/Ch35-Fight.js
+++ src/components/Monad/Ch35-Fight.js
@@ -1,22 +1,31 @@
-// Events starts and we follow Kaja though the event 
+// Events starts and we follow Kaja though the event
 import React from "react";
-import { Content } from "./styles"
+import { Content } from "./styles";
 function Intro(props) {
-return (
-<Content>
-    <h4>Fight!</h4>
-    {/* Showing of the arena and the contestants inside the jungle or walking to the area? */}
-    <p></p>
-    {/* 'Fans' making bets and saying the most retarded things you can think of */}
-    {/* Some retard teacher explaining retarded rules */}
-    {/* The start of the event with people running away, retards running for the middle to battle out the 'strongest' */}
-    {/* tracking of students though the jungles */}
-    {/* Dumb 'events' */}
-    {/* More dumb student banter */}
-    {/* Cornering students and beating them to death */}
-    {/* running away */}
-</Content>
-);
+  return (
+    <Content>
+      <h4>Fight!</h4>
+      <p>
+        The days had gone by since I told I could go back and train. Chu-gong
+        had stayed down there and I didn't offer to meet up with him after, I
+        already had one guy act nice to me just to throw it in my face in the
+        same day.
+      </p>
+      <p>
+        
+      </p>
+      {/* Showing of the arena and the contestants inside the jungle or walking to the area? */}
+      <p></p>
+      {/* 'Fans' making bets and saying the most retarded things you can think of */}
+      {/* Some retard teacher explaining retarded rules */}
+      {/* The start of the event with people running away, retards running for the middle to battle out the 'strongest' */}
+      {/* tracking of students though the jungles */}
+      {/* Dumb 'events' */}
+      {/* More dumb student banter */}
+      {/* Cornering students and beating them to death */}
+      {/* running away */}
+    </Content>
+  );
 }
 export default Intro;
 // It ends half way though the event with Chu-gong needing to die to save Kaja
