447486380515e28a638422252b281f34c938d8fb
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-08-28 18:00:52 +0900

    Update Ch40-War.js

---
+++ src/components/Monad/Ch40-War.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Content } from "./styles"
+function Intro(props) {
+return (
+<Content>
+    <h4>War</h4>
+    {/* Leikans leave */}
+    {/* Off to fight in the war */}
+</Content>
+);
+}
+export default Intro;
