eba9890898345e2d13a40ee3a05a21f07d0187e9
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-09-15 21:23:36 +0900

    Update Ch37-Strange.js

--- src/components/Monad/Ch37-Strange.js
+++ src/components/Monad/Ch37-Strange.js
@@ -18,14 +18,39 @@ function Intro(props) {
       </p>
       <p>"I just kind of thought that's just how it worked."</p>
       <p>
-          "Well your not the first or only to think so. For the longest time if anyone did care their voice was not recorded. We actually owe to parities for our current theories in Alden. The witch of the west and the Leikans. Thanks in part to their, alternative views on subjects. We currently believe that, and pay attention now." He turned and started drawing on the board behind him. A human body at first....
+        "Well your not the first or only to think so. For the longest time if
+        anyone did care their voice was not recorded. We actually owe to
+        parities for our current theories in Alden. The witch of the west and
+        the Leikans. Thanks in part to their, alternative views on subjects. We
+        currently believe that, and pay attention now." He turned and started
+        drawing on the board behind him. A human body at first....
       </p>
       <p>
-          "A level up occurs because you have stored up foreign mana, mana that is not naturally occurring in your body. Your body can no longer handle the excess and starts to adapt the mana to your own, this process changes your body, making your natural mana capacity a little larger along with your health points, that is to say you become more robust. Two questions now, only answer one each when called upon by me. How does it change your body? Where does this foreign mana come from? ***name****"
+        "A level up occurs because you have stored up foreign mana, mana that is
+        not naturally occurring in your body. Your body can no longer handle the
+        excess and starts to adapt the mana to your own, this process changes
+        your body, making your natural mana capacity a little larger along with
+        your health points, that is to say you become more robust. Two questions
+        now, only answer one each when called upon by me. How does it change
+        your body? Where does this foreign mana come from? ***name****"
       </p>
       <p>"I can answer either?"</p>
       <p>"Yes, since you are first."</p>
-      <p></p>
+      <p>
+        "The foreign mana comes from the monsters we kill or from quests though
+        the gods."
+      </p>
+      <p>"Yes, that is the current thought. And ***name***?"</p>
+      <p>"The mana goes into your body, thats how you get stronger."</p>
+      <p>
+        "Very good, the mana does go into your body. If we stick with the
+        strength analogy, the mana breaks down and rebuilds your muscles to
+        increase the mana flow and storage in the muscles, therefor making you
+        stronger.Similar effects happen for the other stats. So the stat points are a display of how much foreign manner you have to be distributed. So what happens if you save up your stat points?"
+      </p>
+      <p>
+          ""
+      </p>
       <div className="BreakPoint"></div>
       <SeaPeopleFunction Chapter={7} />
     </Content>
