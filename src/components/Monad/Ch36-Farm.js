9f9f68002ad5c9c0e8da13d56f11d19756f49ed1
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-10-01 22:20:25 +0900

    Update Ch36-Farm.js

--- src/components/Monad/Ch36-Farm.js
+++ src/components/Monad/Ch36-Farm.js
@@ -23,6 +23,107 @@ function Intro(props) {
         floor between the riddle of vines. As for the plants I did know, there
         was corn in neat little squares past the strange plants. Odd.
       </p>
+      <p>
+        I wanted to sneak past the plants but everything in my body was
+        screaming that was a trap. I decided to keep edging around to see if I
+        could see a way inside. Then I saw another student. Black robe with
+        yellow trim, a lightning mage. They were standing within one of the corn
+        fields looking at the plants, pulling them down, close to their face each
+        time.
+      </p>
+      <p>
+        "Don't worry. The plants wont harm you. If you tell me your name." The
+        student said. His voice was deep, so I could tell he was a boy but since
+        he wore his academy robe with the hood over his head I could not see his
+        face. I did not miss the subtle hint he dropped, did he control the
+        plants around here? He must be a lightning mage, students elements could
+        not be miscategorised. Did he kill another student and wear their robe?
+        Why? The robes helped the element you were.
+      </p>
+      <p>
+        If he was really a plant and nature element then I had the elemental
+        advantage. Without changing his routeing of looking at corn he spoke
+        out. "You must be hungry. Tell me your name and I'll let you come in and
+        feed you my corn. I don't want to trick or kill you. I want to end this
+        silly game. All we have to do is all agree not to kill each other. They
+        can't force us to kill each other." His voice was not soft, but it was
+        not harsh, it just sounded almost hopeless.
+      </p>
+      <p>
+        "You don't sound or look like someone who wants to sit things out
+        quietly. You sound bored like someone who has tricked many people to
+        their death." I accused him.
+      </p>
+      <p>
+        "Ahaha. Do I really?" He turned his body to stop playing with his corn
+        finally facing me properly. I reached up with both hands and I hide
+        behind the tree trunk completely. When I peeked again he had pulled down
+        his hood. He looked, average. His face was marked with acne, his hair
+        was dark brown with muk, although I am sure mine was not clean either.
+        His eyes were hazel, accompanied by naturally long eye lashes.
+      </p>
+      <p>
+        Did he think I would trust him if he showed me his face? Pfft. "My names
+        Otto." He shouted to me. He was so common, Aldenese name, look, accent.
+        Not a single magic item on him anywhere. I guess he could keep them in
+        his inventory but he should at least have his weapon hidden somewhere in
+        these plants.
+      </p>
+      <p>"I don't give a rats what your name is." I spat back.</p>
+      <p>"Corn tastes better then rat." He replied.</p>
+      <p>
+        What? I never said I ate them. Was he mocking me? "It's just your name.
+        I'm not asking for your weakness." He continued.
+      </p>
+      <p>
+        "Kaja." I told him. He was right, it's not like he could do anything
+        with my name.
+      </p>
+      <p>
+        "Really? Kaja the first year fire mage. Didn't you beat me on our 5th
+        day here? The duels we did back in the arena." He said.
+      </p>
+      <p>
+        "I've fought lots of people in the arena. I..." I guess he could of
+        been. The name does ring a bell in mind now that he has said it. I was
+        so excited back then I guess I forgot who I fought. "So what?" I
+        shouted.
+      </p>
+      <p>
+        "Well, we already know the outcome of our fight." He shrugged. "Some
+        come eat some corn. With your fire we could cook it, that be a nice
+        change from cold food." He replied while plucking two of the corn from
+        the plant near him and walking back to the cabin.
+      </p>
+      <p>
+        When he left I had time to slow down and think. Well he was a lightning
+        mage, I've seen his element before personally. Not a plant mage, so no
+        way he could control the plants. I stand a better chance fighting the
+        plants then turning back. One opponent is easier then a group, it makes
+        no sense not to follow him, even if I don't trust him right?
+      </p>
+      <p>Time passed. My belly rumbled.</p>
+      <p>
+        I walked out slowly between the vines, nothing happened so far, so I
+        tip-toed across the vines staying clear of the large red flowers. Not
+        long later I reached the field of corn Otto was stood earlier. I
+        followed in his foot steps towards the cabin.
+      </p>
+      <p>
+        Squatting down just outside the entrance he was facing away from me, the
+        perfect time to strike. I lit up the fire in my hand. "I'm glad you
+        came." He said without turning around which caught me off guard. "I'm
+        not normally a vegan but I cannot eat this meat raw so I'm glad you came
+        really." He turned back holding up a basket made of wicker? Inside was
+        pieces of meat. Even now would be a good time to strike. Was he serious
+        about his plan? Surely not.
+      </p>
+      <p>
+        "It's rude to kill a guest you invited to eat, but since you don't trust
+        me we can sit facing each other if you like. I just want you to hear me
+        out. But mostly I want your fire to cook." He said. It's been day and
+        night can he really not go that long without eating?
+      </p>
       <p></p>
       {/* Otto Vs Morons */}
       {/* Living in the farm */}
