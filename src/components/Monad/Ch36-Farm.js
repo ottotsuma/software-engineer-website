afd291ffcf7878160d1a67053d8fd89386d41302
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-10-01 15:35:51 +0900

    Update Ch36-Farm.js

--- src/components/Monad/Ch36-Farm.js
+++ src/components/Monad/Ch36-Farm.js
@@ -1,24 +1,42 @@
 //  Picks up from Kaja being saved to finding Otto's grotto
 import React from "react";
-import { Content } from "./styles"
+import { Content } from "./styles";
 function Intro(props) {
-return (
-<Content>
-    <h4>Farm!?</h4>
-    {/* Found Otto's 'Farm' */}
-    {/* Otto Vs Morons */}
-    {/* Living in the farm */}
-    {/* Last of the morons */}
-    {/* it's love! ... No it's not */}
-    {/* Strange class */}
-    {/* Nobles argue */}
-    {/* Leikans leave */}
-    {/* Off to fight in the war */}
-    <p></p>
-</Content>
-);
+  return (
+    <Content>
+      <h4>Farm!?</h4>
+      {/* Found Otto's 'Farm' */}
+      <p>
+        I must be out of bounds, or the comity did not find this before the
+        event started. It was a primate looking log cabin, it would be better to
+        call it a tree trunk cabin since it was made of uncut tree trunks just
+        piled on top of each other. Parts of it were riddle with rot and all
+        things that crawl. While I was in need of a place to stay I was not just
+        gonna rush straight in there.
+      </p>
+      <p>
+        I moved from tree to tree peeking out from behind. The cottage had tree
+        stumps around it, must of been the trees used to make it. The
+        surrounding area had various plants I had not seen before, and some I
+        had. There were large vines with bulbous orange pustules pulsing out
+        from them. Large red flowers, half the size of a person sitting on the
+        floor between the riddle of vines. As for the plants I did know, there
+        was corn in neat little squares past the strange plants. Odd.
+      </p>
+      <p></p>
+      {/* Otto Vs Morons */}
+      {/* Living in the farm */}
+      {/* Last of the morons */}
+      {/* it's love! ... No it's not */}
+      {/* Strange class */}
+      {/* Nobles argue */}
+      {/* Leikans leave */}
+      {/* Off to fight in the war */}
+      <p></p>
+    </Content>
+  );
 }
 export default Intro;
 // The event ends with the organizers forcing an end since Kaja and Otto refuse to fight and wont die of starvation. They claim it shows how great the students of Alden are.
 
-// 
+//
