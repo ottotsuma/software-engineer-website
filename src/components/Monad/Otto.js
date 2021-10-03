84da231f975283cc146ed2ea20a26c4ac1b441ec
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-10-03 18:40:09 +0900

    Update Otto.js

--- src/components/Monad/Otto.js
+++ src/components/Monad/Otto.js
@@ -3,10 +3,148 @@ import Item from "./../elements/item";
 import Equipment from "./../elements/equipment";
 import Stats from "./../elements/stats";
 import Spells from "./../elements/spells";
+// Trying cards again
+import {spellList} from "./../elements/spells"
+import styled from "styled-components";
+import {imageError} from './../color'
+
+const elementList = {
+  "lightning" : {
+    color: "yellow",
+    image: 'https://i.pinimg.com/originals/22/86/5d/22865d4b8b7e517dce1b60a2988c8482.gif',
+  },
+  "fire" : {
+    color: "orange",
+    image: "https://i.pinimg.com/originals/4d/ca/d9/4dcad98ff2d9aba671b56957ab5d70a2.gif",
+  },
+  "blood" : {
+    color: "red",
+    image: "https://github.com/ottotsuma/ottotsuma.github.io/blob/master/images/spells%20effects/b323b0af13f20645569964f9c75b4909.gif?raw=true",
+  }
+}
+
+function MakeCard (name, level, element) {
+  const elementalColor = elementList[element].color
+  const spellName = spellList[name].name
+  const spellDisc = spellList[name][level]
+  const elementImage = elementList[element].image
+  return (
+    <CardContainer>
+    <Card element={elementalColor}>
+      <TitleWrap><CardTitle>{spellName}</CardTitle><TitleSpan>{spellName}</TitleSpan></TitleWrap>
+      <CardElement onError={imageError} src={elementImage}></CardElement>
+      <DiscWrap><CardDisc>{spellDisc}</CardDisc><DiscSpan>{spellDisc}</DiscSpan></DiscWrap>
+      <CardSub>Level: {level}</CardSub>
+    </Card>
+  </CardContainer>
+  )
+}
+
+const DiscSpan = styled.span`
+  visibility: hidden;
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px;
+  width: auto;
+  position: absolute;
+  z-index: 2;
+  margin-top: 50px;
+`;
+
+const TitleSpan = styled.span`
+  visibility: hidden;
+  background-color: black;
+  color: #fff;
+  text-align: center;
+  border-radius: 6px;
+  padding: 5px;
+  width: auto;
+  position: absolute;
+  z-index: 2;
+`;
+
+const CardContainer = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+resize: horizontal;
+`;
+const Card = styled.div`
+border: solid;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+background: white;
+box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+border-radius: 0.5rem;
+margin: 0.5rem;
+border-width: thick;
+border-color: ${props => props.element ? props.element : 'black'};
+justify-content: space-around;
+height: 300px;
+width: 150px;
+padding: 1%;
+`;
+const CardTitle = styled.h2`
+width: 100%;
+text-overflow: ellipsis;
+overflow: hidden;
+white-space: nowrap;
+`;
+const TitleWrap = styled.div`
+width: 100%;
+display: flex;
+&:hover ${TitleSpan} {
+  visibility: visible;
+}
+`;
+
+const CardElement = styled.img`
+width: 100%;
+height: 150px;
+object-fit: cover;
+border-radius: 1rem;
+@media screen and (max-width: 300px) {
+  display: none;
+}
+margin-top: 2%;
+`;
+const CardDisc = styled.div`
+overflow: hidden;
+text-overflow: ellipsis;
+display: -webkit-box;
+-webkit-line-clamp: 2;
+-webkit-box-orient: vertical;
+`;
+const DiscWrap = styled.div`
+width: 100%;
+display: flex;
+&:hover ${DiscSpan} {
+  visibility: visible;
+}
+`;
+const CardSub = styled.div`
+margin-top: 2%;
+`;
+
+const CardHand = styled.div`
+display: flex;
+flex-wrap: wrap;
+justify-content: center;
+`;
 
 export default function Otto() {
   return (
     <div style={{ color: "black" }}>
+      <CardHand>
+      {MakeCard('Sparks', 1, 'lightning')}
+      {MakeCard('Fireball', 1, 'fire')}
+      {MakeCard('Blood Hammer Drill', 1, 'blood')}
+      </CardHand>
       <Stats
         type={"description"}
         stats={{
@@ -122,3 +260,5 @@ export default function Otto() {
 // A bat and death god.
 
 // God K. God L is often combined with god K, the lightning deity who, as an owner of the seeds, was considered a bringer of abundance.[5] More specifically, god L can extend the head of god K, or carry an infant god K in a sling on his back.[6] God K also happens to be the victim of god L as a Venus patron (Dresden Codex).
+
+
