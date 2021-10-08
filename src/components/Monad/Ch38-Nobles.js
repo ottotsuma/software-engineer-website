d9e0cf578ad8ee4467908f9185fd5bbdb006b944
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-10-08 13:13:44 +0900

    Update Ch38-Nobles.js

--- src/components/Monad/Ch38-Nobles.js
+++ src/components/Monad/Ch38-Nobles.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { Content } from "./styles"
+import { Content } from "./styles";
 import SeaPeopleFunction from "./SeaPeople"; // 8
 
 function Intro(props) {
-return (
-<Content>
-        <SeaPeopleFunction Chapter={8} />
-<div className="BreakPoint"></div>
-    <h4>The Nobles</h4>
-    {/* Nobles argue */}
-    {/* Leikans leave */}
-    {/* Off to fight in the war */}
-    <p></p>
-</Content>
-);
+  return (
+    <Content>
+      <h4>Juice Up</h4>
+      <SeaPeopleFunction Chapter={8} />
+      <div className="BreakPoint"></div>
+      <h4>The Nobles</h4>
+      {/* Nobles argue */}
+      {/* Leikans leave */}
+      {/* Off to fight in the war */}
+      <p></p>
+    </Content>
+  );
 }
 export default Intro;
-// Obsidian is the royals resource 
+// Obsidian is the royals resource
