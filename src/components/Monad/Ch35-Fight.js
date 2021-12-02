c17a53032aafff5b27aa1b43040a2d9793e90433
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2021-12-02 20:50:22 +0900

    Update Ch35-Fight.js

--- src/components/Monad/Ch35-Fight.js
+++ src/components/Monad/Ch35-Fight.js
@@ -575,7 +575,10 @@ function Intro(props) {
       </p>
       <p>
         I just kept wanting to get further away. If I couldn't see them when I
-        looked back, they could not see me either. 
+        looked back, they could not see me either. The further I got from the
+        middle the safer I felt. It was silly really, no where was really safe.
+        I guess my best outcome is to hope I run into no one else until near the
+        end so my chances of being saved by professor Farald are higher.
       </p>
       {/* running away */}
     </Content>
