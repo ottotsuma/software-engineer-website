f8de85f1ed35eff50fdb3882fe4d7f78157e9266
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2022-02-01 21:00:56 +0900

    Update Ch13-Savigal.js

--- src/components/Monad/Ch13-Savigal.js
+++ src/components/Monad/Ch13-Savigal.js
@@ -296,7 +296,10 @@ function Intro(props) {
             instructor, a fighter who had changed his class to swordsman at
             level 10. Now he was level 40 his level would be pull back down to
             10 for the duration of the dungeon. Anneliese was a level 6 fighter.
-            Finally Klemens was a level 5 rouge. Since they had not reached level 10 yet, non of us them unlocked any new classes.
+            Finally Klemens was a level 5 rouge. Since they had not reached level 10 yet, neither of them unlocked any new classes.
+          </p>
+          <p>
+            
           </p>
           {/* 11 ~ 14 */}
           {/* Wandering Zombie,  
