62a7f0567420cb1ddda2bde088f808a7cf6840a1
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2022-03-27 19:53:27 +0900

    Update Hitori.js

--- src/components/Monad/Hitori.js
+++ src/components/Monad/Hitori.js
@@ -85,6 +85,18 @@ export const HitoriStory = {
         everything they would go through in the dungeon, these excited teenagers
         would no longer differ from them.
       </Hitori>
+      <Hitori>
+        The first time I was the same, well maybe not as energetic as this lot. I had a drive to test my new powers, get rich quick but life wasn't like that, no. Life was too far from what I imagined.
+      </Hitori>
+      <Hitori>
+        After seeing so many of my friends and companions die in front of me without being able to do anything, once they see, they too will finally understood how weak and insignificant they really are. This was the difficult life of a lower classes, students with Rank F and E like themselves. Different from the life of students Rank B, A, or S.
+      </Hitori>
+      <Hitori>
+        Sitting just behind the group playing slaps were some teenage girls, since they were in the seats in front of me it was hard not to over hear their whispering.
+      </Hitori>
+      <p>"What is it, Hide?" The friend asked.</p>
+      <p>"You weren't planning on going to talk to him, Utano?" Hide asked her in a light whisper.</p>
+      <p>"Do you think he's cute too?" Utano replied quickly.</p>
     </Content>
   ),
 };
