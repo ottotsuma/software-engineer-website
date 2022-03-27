a928973f99e6635ab4de7a5c467f74d8aff03a3d
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2022-03-27 18:40:25 +0900

    Update Hitori.js

--- src/components/Monad/Hitori.js
+++ src/components/Monad/Hitori.js
@@ -60,17 +60,30 @@ export const HitoriStory = {
     <Content style={{ overflowWrap: "anywhere" }}>
       <h1>Hitori</h1>
       <Hitori>
-        On a long road, our large and exquisite black wagon thundered along
-        in the heat alone. Inside, a group of people accompanying me were doing
+        On a long road, our large and exquisite black wagon thundered along in
+        the heat alone. Inside, a group of people accompanying me were doing
         different things, completely ignoring the picturesque view just outside
         the window.
       </Hitori>
       <Hitori>
         Endless grass spread out to the horizon. Petite flowers of various
-        colours broke up the endless green. Inside the wagon some teenagers
-        were playing slaps, the noise they were making while laughing and having
-        fun, pushed out any chance for me to catch up on lost sleep. I enjoyed
-        the night shifts solitude, but this was the payment I had to make.
+        colours broke up the endless green. Inside the wagon some teenagers were
+        playing slaps, the noise they were making while laughing and having fun,
+        pushed out any chance for me to catch up on lost sleep. I enjoyed the
+        night shifts solitude, but this was the payment I had to make.
+      </Hitori>
+      <Hitori>
+        At the back side of the wagon, some older men and women were sitting
+        without making a noise. They all sat back, their eyes closed with
+        passive faces. Sometimes it was possible to see a few of them sighing.
+        The difference between the group who had experienced this before and the
+        group of teenagers who were going there for the first time.
+      </Hitori>
+      <Hitori>
+        But even with the teenagers being very noisy, the adults did not voice
+        their complaints. They, like me, knew that on the way back, after
+        everything they would go through in the dungeon, these excited teenagers
+        would no longer differ from them.
       </Hitori>
     </Content>
   ),
