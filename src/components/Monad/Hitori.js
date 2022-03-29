601ede08231bd3d6d8ad88105dcf3f229ef78bbb
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2022-03-29 19:25:05 +0900

    Update Hitori.js

--- src/components/Monad/Hitori.js
+++ src/components/Monad/Hitori.js
@@ -91,8 +91,8 @@ export const HitoriStory = {
         After seeing so many of my friends and companions die in front of me
         without being able to do anything, once they see, they too will finally
         understood how weak and insignificant they really are. This was the
-        difficult life of a lower classes, students with Rank F and E like
-        themselves. Different from the life of students Rank B, A, or S.
+        difficult life of a lower classes, students with rank F and E like
+        themselves. Different from the life of students rank B, A, or S.
       </Hitori>
       <Hitori>
         Sitting just behind the group playing slaps were some teenage girls,
@@ -144,7 +144,7 @@ export const HitoriStory = {
         menu and focused on then new skill I had unlocked but had not used yet,
         a part of me was hoping I would never have to use it.
       </Hitori>
-      <div>{SpellCards([["Rebirth Flame", 1, 'fire']])}</div>
+      <div>{SpellCards([["Rebirth Flame", 1, "fire"]])}</div>
       {HitoriStats(1)}
       {/* Fire bird boy, revives on death. */}
     </Content>
@@ -171,13 +171,13 @@ export const HitoriStory = {
         this way.
       </Hitori>
       <Hitori>
-        In my year, there was even a boy who scored a Rank B, and as expected,
+        In my year, there was even a boy who scored a rank B, and as expected,
         he became the school idol. Of course, the school's head master didn't
         miss the opportunity to publicize it throughout the city that in his
-        school a student had achieved a B Rank.
+        school a student had achieved a B rank.
       </Hitori>
       <Hitori>
-        Of course, just receiving a Rank B wouldn't turn a person into a monster
+        Of course, just receiving a rank B wouldn't turn a person into a monster
         with the power to destroy a village. No, all the students started from
         the bottom, but the amount of help you would receive and what you could
         do in the future was affected by your rank. Since your student rank
@@ -200,50 +200,105 @@ export const HitoriStory = {
         could be re-evaluated as a low rank F instead of a useless rank F.
       </Hitori>
       <Hitori>
-        The ride trundled on until we arrived at the dungeon. The other teenagers were the first to run off the bus, the adults were far less willing. The dungeon, this dungeon you could smell before you saw it. It was a sewages system from a previous civilization. The entrance was a man sized hole in the ground. Standing near by was a soldier with a list in his hand.
+        The ride trundled on until we arrived at the dungeon. The other
+        teenagers were the first to run off the bus, the adults were far less
+        willing. The dungeon, this dungeon you could smell before you saw it. It
+        was a sewages system from a previous civilization. The entrance was a
+        man sized hole in the ground. Standing near by was a soldier with a list
+        in his hand.
       </Hitori>
       <Hitori>
-        The list was a list of our names. Once we were checked off the soldier left us to our own devices. The organization of the team was left to be decided by ourselves; along with our safety inside the dungeon. As soon as the soldier had walked to one side the teen named ** already started walking towards the entrance, completely ignoring the existence of others. Apparently this was already something normal, since right after he took the first step, the other 6 teenagers followed him entering the dungeon, all while ignoring the adults and me.
+        The list was a list of our names. Once we were checked off the soldier
+        left us to our own devices. The organization of the team was left to be
+        decided by ourselves; along with our safety inside the dungeon. As soon
+        as the soldier had walked to one side the teen named ** already started
+        walking towards the entrance, completely ignoring the existence of
+        others. Apparently this was already something normal, since right after
+        he took the first step, the other 6 teenagers followed him entering the
+        dungeon, all while ignoring the adults and me.
       </Hitori>
       <Hitori>
-        The adults, on the other hand, started to exchange some information and decide tactics. In the end, the woman with the one eye, was determined to be the leader of the group. When almost everything was decided, she looked at me and commented, "You're not as hot headed as the other young ones, what are your thoughts?."
+        The adults, on the other hand, started to exchange some information and
+        decide tactics. In the end, the woman with the one eye, was determined
+        to be the leader of the group. When almost everything was decided, she
+        looked at me and commented, "You're not as hot headed as the other young
+        ones, what are your thoughts?."
       </Hitori>
       <Hitori>
-        Some adults looked at him surprised, maybe knowing how weak I was, while others looked at him approving the decision. I knew what she meant, since this was not the first time we were doing a dungeon together. 
+        Some adults looked at him surprised, maybe knowing how weak I was, while
+        others looked at him approving the decision. I knew what she meant,
+        since this was not the first time we were doing a dungeon together.
       </Hitori>
       <Hitori>
-        While many thought that I was only afraid of monsters and didn't have the courage to do anything, others with more insightful vision realized countless times that I was never caught in a trap and had several times given valuable information during the raid. That is, I knew much more about the dungeon I invaded than most normal people.
+        While many thought that I was only afraid of monsters and didn't have
+        the courage to do anything, others with more insightful vision realized
+        countless times that I was never caught in a trap and had several times
+        given valuable information during the raid. That is, I knew much more
+        about the dungeon I invaded than most normal people.
       </Hitori>
       <Hitori>
-        While most students spent time reading up on their class and skills. I could not. There was no book about my class, no guide for which skills to pick and combine; so instead I spent that time reading up on the next dungeon I would be sent to. The monsters found inside, the layout and the random factors that have occurred within. Since I had no guide to my skills for survival, I had to survive using my knowledge of the dungeon.
+        While most students spent time reading up on their class and skills. I
+        could not. There was no book about my class, no guide for which skills
+        to pick and combine; so instead I spent that time reading up on the next
+        dungeon I would be sent to. The monsters found inside, the layout and
+        the random factors that have occurred within. Since I had no guide to my
+        skills for survival, I had to survive using my knowledge of the dungeon.
       </Hitori>
       <Hitori>
-          Looking back at the one-eyed woman, I calmly replied. "This dungeon is the home of the King of Rats. Inside there is a huge maze of tunnels, with rats the size of ponies roaming within. Most probably, the overly confident teenagers will end up walking around aimlessly inside. Since the rats' sense of smell is very acute, we have practically gained an incredible number of 7 lures to use. Honestly, for me, we can use those 7 to attract as many rats as possible and end their numbers little by little, since if we go to fight the Rat King with a lot of rats still living, when he goes into berserk mode we will be killed."
+        Looking back at the one-eyed woman, I calmly replied. "This dungeon is
+        the home of the King of Rats. Inside there is a huge maze of tunnels,
+        with rats the size of ponies roaming within. Most probably, the overly
+        confident teenagers will end up walking around aimlessly inside. Since
+        the rats' sense of smell is very acute, we have practically gained an
+        incredible number of 7 lures to use. Honestly, for me, we can use those
+        7 to attract as many rats as possible and end their numbers little by
+        little, since if we go to fight the Rat King with a lot of rats still
+        living, when he goes into berserk mode we will be killed."
       </Hitori>
       <Hitori>
-          Those who had not fought along side be before seemed surprised by my insight, and willingness to use my class mates as decoys. That was their mistake, they were not my classmates, anyone my own age had long since graduated from dungeon introductions, going into dungeons with higher level adults for protection, or simply become a non-combatant.
+        Those who had not fought along side be before seemed surprised by my
+        insight, and willingness to use my class mates as decoys. That was their
+        mistake, they were not my classmates, anyone my own age had long since
+        graduated from dungeon introductions, going into dungeons with higher
+        level adults for protection, or simply become a non-combatant.
       </Hitori>
       <Hitori>
-          Moving down into the sewer system a terrible smell of feces and rotten things hanged in the air. If it was the first time I felt this, I might have felt sick, but having passed through this dungeon at least 3 times in the last 2 years, the smell no longer affected me as much.
+        Moving down into the sewer system a terrible smell of feces and rotten
+        things hanged in the air. If it was the first time I felt this, I might
+        have felt sick, but having passed through this dungeon at least 3 times
+        in the last 2 years, the smell no longer affected me as much.
       </Hitori>
       <Hitori>
-        Unfortunately, the same could not be said for my new companions. Looking at the ground, it was possible to see a pool of vomit, most, likely left behind by those teenagers. Fortunately for us this meant they had not got too far after entering, making them easier to trail. After awhile everyone had noticed the same, the only noise was our own footsteps, everyone in our group had ran this dungeon at least once. I imagine some of the adults here had run this dungeon almost a hundred times.
+        Unfortunately, the same could not be said for my new companions. Looking
+        at the ground, it was possible to see a pool of vomit, most, likely left
+        behind by those teenagers. Fortunately for us this meant they had not
+        got too far after entering, making them easier to trail. After awhile
+        everyone had noticed the same, the only noise was our own footsteps,
+        everyone in our group had ran this dungeon at least once. I imagine some
+        of the adults here had run this dungeon almost a hundred times.
       </Hitori>
-{/* Farming the resources from the dungeons by sending people in over and over. */}
-"So what if it's not making noise?" A fat man asked in a presumptuous tone. It was he who proposed to go straight to kill the King of the Rats, but Noah refuted the idea he gave and left him with a sour mood.
-
-Noah looked at him with a disappointed look and shook his head. He knew the fat man was a middle-aged Blessed Rank E who never managed to actually reach the strength of Rank E, being forced to always invade Rank F Fortresses because he was too weak to advance Rank. Even the man, being more than 30 years old, never bothered to learn or study the Fortresses he invaded. Hence came the disappointment Noah had for the man who only depended on having a stronger Blessing to survive until today.
-
-"Are you dumb?" asked the man with the scar while looking at the fat man. "The most striking feature of the King of the Rats Fortress is the wind that runs in here at such a high speed that it seems to be whistling. Can you hear any whistling by chance?"
-
-Hearing the scar man's question, the fat man finally noticed his own folly and realized that the whistling noise he always heard around here was not present this time. "What's happening?" He asked with a little fear.
-
-Noah wanted an opportunity to get away from the group without arousing suspicion and test the blessing he received from Lucifer, and now the opportunity had fallen right in his lap. "Something must be wrong. I'm going for a walk through the Fortress to look for it; you guys keep an eye out for the 7 baits."
-
-"Don't die needlessly." Said the man with the scar. Taking a final look at Noah, he and the other adventures chased after the vomit trail. After going a few times to Fortresses with Noah, he knew this boy would not die here even if something abnormal happened.
-
-However, nobody knew that something that could really kill anyone there was truly happening inside that Fortress…
-
+      <Hitori>
+        "So what if it's not making noise?" A fat man asked in a presumptuous
+        tone. I knew the fat man was a middle-aged who should of become a rank
+        E, however not everyone achieves their predicted rank. Since he was rank
+        F he was being forced to always invade rank F dungeons because he was
+        too weak to advance rank. Even the man, being more than 30 years old,
+        never bothered to learn or study the dungeons he invaded. He always left
+        me with the bitter taste of disappointment.
+      </Hitori>
+      <Hitori>
+        "Are you short a few marbles?" asked the woman with one-eye. "The most
+        striking feature of the King of the Rats dungeon is the wind that runs
+        in here at such a high speed that it seems to be whistling. Can you hear
+        any whistling by chance?"
+      </Hitori>
+      <Hitori>
+        Hearing the womans's question, the fat man finally noticed his own folly
+        and realized that the whistling noise he always heard around here was
+        not present this time. "What's happening?" He asked with a little fear.
+      </Hitori>
+      <Hitori>"Something must be wrong." I muttered.</Hitori>
+      {/* Farming the resources from the dungeons by sending people in over and over. */}
     </Content>
   ),
 };
