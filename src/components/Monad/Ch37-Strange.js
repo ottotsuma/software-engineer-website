37e55db41327ce1a36a00c44f01849aa3274083f
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2022-04-10 19:47:39 +0900

    Update Ch37-Strange.js

--- src/components/Monad/Ch37-Strange.js
+++ src/components/Monad/Ch37-Strange.js
@@ -13,16 +13,17 @@ function Intro(props) {
       <p>
         "Now that you are all at a point were you understand the very basics,
         it's time for you to learn the next step. Understanding the fundamentals
-        of why it works. Why when you put attribute points into Strength do you get
-        stronger? Why do you get attribute points at level ups? Why do you level up?"
+        of why it works. Why when you put attribute points into Strength do you
+        get stronger? Why do you get attribute points at level ups? Why do you
+        level up?"
       </p>
       <p>"I just kind of thought that's just how it worked."</p>
       <p>
         "Well your not the first or only to think so. For the longest time if
         anyone did care their voice was not recorded. We actually owe to
         parities for our current theories in Alden. The witch of the west and
-        the Niyoshons. Thanks in part to their, alternative views on subjects. We
-        currently believe that, and pay attention now." He turned and started
+        the Niyoshons. Thanks in part to their, alternative views on subjects.
+        We currently believe that, and pay attention now." He turned and started
         drawing on the board behind him. A human body at first....
       </p>
       <p>
@@ -46,32 +47,32 @@ function Intro(props) {
         "Very good, the mana does go into your body. If we stick with the
         strength analogy, the mana breaks down and rebuilds your muscles to
         increase the mana flow and storage in the muscles, therefor making you
-        stronger.Similar effects happen for the other attributes. So the attribute points
-        are a display of how much foreign manner you have to be distributed. So
-        what happens if you save up your attribute points?"
+        stronger.Similar effects happen for the other attributes. So the
+        attribute points are a display of how much foreign manner you have to be
+        distributed. So what happens if you save up your attribute points?"
       </p>
       <p>
-        "Nothing? I stored up attribute points a few times and nothing changed.."
-        Otto replied after being picked.
+        "Nothing? I stored up attribute points a few times and nothing
+        changed.." Otto replied after being picked.
       </p>
       <p>
         "On the surface yes. However, this is a real lesson you should be
         mindful of. The system displays some attributes to you, but nothing
-        everything about your body and your mana. Some of the attributes are hidden.
-        Let me give you some examples. Can anyone tell me their HP regeneration
-        rate per second? Your spell casting time reduction? Experience points
-        increase or decrease percentage?"
+        everything about your body and your mana. Some of the attributes are
+        hidden. Let me give you some examples. Can anyone tell me their HP
+        regeneration rate per second? Your spell casting time reduction?
+        Experience points increase or decrease percentage?"
       </p>
       <p>
-        ****[teacher] waited for long enough that the silence almost felt awkward before
-        saying "These are things that the system will not tell you directly. You
-        have not read the studies conducted on these topics, but I have. Should
-        you trust me then I can report that there are negative effects
-        associated with storing up the spare mana inside your body. The body
-        tries it's best to distribute the mana in the absences of your guidance
-        so there are effects. Don't let this dissuade you from saving up your
-        points of course. Just a little something to keep in mind before a life
-        and death situation."
+        ****[teacher] waited for long enough that the silence almost felt
+        awkward before saying "These are things that the system will not tell
+        you directly. You have not read the studies conducted on these topics,
+        but I have. Should you trust me then I can report that there are
+        negative effects associated with storing up the spare mana inside your
+        body. The body tries it's best to distribute the mana in the absences of
+        your guidance so there are effects. Don't let this dissuade you from
+        saving up your points of course. Just a little something to keep in mind
+        before a life and death situation."
       </p>
       <div className="BreakPoint" />
       <SeaPeopleFunction Chapter={7} />
