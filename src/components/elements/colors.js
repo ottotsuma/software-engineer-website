a4076af7ad1646099894dec1ed832d4b086c36cd
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2022-05-01 22:17:40 +0900

    Update colors.js

--- src/components/elements/colors.js
+++ src/components/elements/colors.js
@@ -1,112 +1,131 @@
 export const colors = {
-    // blue: "#0d89eb",
-    // lightBlue: "#2CA8FF",
-    // darkBlue: "#161b21",
-    // red: "#f92b00",
-    // lightRed: "#ff5745",
-    // green: "#54cc4f",
-    // offWhite: '#CCCCCC',
-    // offBlack: '#1A1A1A',
-    // grey: '#474747',
-    lightBackground: `linear-gradient(0deg, rgba(34,89,195,0.7) 9%, rgba(45,253,209,0.3) 100% )`,
-    electricLime: '#c1ff00',
-    yellow: {
-        electric: '#F7F731'
-    },
-    brown: {
-        Murasakitobi: '#512c31'
-    },
-    black: {
-        normal: '#000',
-        off: '#1A1A1A',
-    },
-    white: {
-        normal: '#fff',
-        off: '#CCCCCC',
-    },
-    grey: {
-        tab: '#adb7c5',
-        lighter: '#6e7b8b',
-        light: '#47515d', //In the SCSS variables is called: grey-hover
-        normal: '#2f353d',
-        oldNormal: '#474747',
-        medium: '#23292f',
-        dark: '#161b21',
-    },
-    orange: {
-        light: '#fd9d20',
-        normal: '#ff7d08',
-        dark: '#ba6108',
-    },
-    blue: {
-        lighter: '#b3ddff',
-        light: '#46adff',
-        normal: '#0d89eb',
-        dark: '#076fc1',
-        darker: '#1a3b5b',
-    },
-    red: {
-        light: '#ff5745',
-        normal: '#f92b00',
-        dark: '#b21100',
-    },
-    green: {
-        light: '#32db86',
-        normal: '#28a968',
-        dark: '#1b8750',
-        darker: '#00663f',
-    },
-    purple: {
-        normal: 'purple',
-    },
-    meta: {
-        light: '#fff',
-        normal: '#d8d8d8',
-        dark: '#bfbfbf',
-    },
-    social: {
-        facebook: '#3b5998',
-        twitter: '#1da1f2',
-        youtube: '#cd201f',
-        twitch: '#6441a5',
-        line: '#00c300',
-    },
-    text: {
-        normal: '#ededed',
-        textLight: '#ededed',
-        textStrong: '#ededed',
-        textSelection: '#0d89eb',
-    },
-    primary: '#ff7d08',
-    bodyBackground: '#2f353d',
-    background: '#2f353d',
-    linkVisited: '#ff7d08',
-    linkHover: '#fd9d20',
-    linkInvert: '#ff7d08',
+  // blue: "#0d89eb",
+  // lightBlue: "#2CA8FF",
+  // darkBlue: "#161b21",
+  // red: "#f92b00",
+  // lightRed: "#ff5745",
+  // green: "#54cc4f",
+  // offWhite: '#CCCCCC',
+  // offBlack: '#1A1A1A',
+  // grey: '#474747',
+  lightBackground: `linear-gradient(0deg, rgba(34,89,195,0.7) 9%, rgba(45,253,209,0.3) 100% )`,
+  electricLime: "#c1ff00",
+  yellow: {
+    electric: "#F7F731",
+  },
+  brown: {
+    Murasakitobi: "#512c31",
+  },
+  black: {
+    normal: "#000",
+    off: "#1A1A1A",
+  },
+  white: {
+    normal: "#fff",
+    off: "#CCCCCC",
+  },
+  grey: {
+    tab: "#adb7c5",
+    lighter: "#6e7b8b",
+    light: "#47515d", //In the SCSS variables is called: grey-hover
+    normal: "#2f353d",
+    oldNormal: "#474747",
+    medium: "#23292f",
+    dark: "#161b21",
+  },
+  orange: {
+    light: "#fd9d20",
+    normal: "#ff7d08",
+    dark: "#ba6108",
+  },
+  blue: {
+    lighter: "#b3ddff",
+    light: "#46adff",
+    normal: "#0d89eb",
+    dark: "#076fc1",
+    darker: "#1a3b5b",
+  },
+  red: {
+    light: "#ff5745",
+    normal: "#f92b00",
+    dark: "#b21100",
+  },
+  green: {
+    light: "#32db86",
+    normal: "#28a968",
+    dark: "#1b8750",
+    darker: "#00663f",
+  },
+  purple: {
+    normal: "purple",
+  },
+  meta: {
+    light: "#fff",
+    normal: "#d8d8d8",
+    dark: "#bfbfbf",
+  },
+  social: {
+    facebook: "#3b5998",
+    twitter: "#1da1f2",
+    youtube: "#cd201f",
+    twitch: "#6441a5",
+    line: "#00c300",
+  },
+  text: {
+    normal: "#ededed",
+    textLight: "#ededed",
+    textStrong: "#ededed",
+    textSelection: "#0d89eb",
+  },
+  primary: "#ff7d08",
+  bodyBackground: "#2f353d",
+  background: "#2f353d",
+  linkVisited: "#ff7d08",
+  linkHover: "#fd9d20",
+  linkInvert: "#ff7d08",
 };
 export const monadColors = {
-    junk: "rgb(109, 109, 109)", // grey
-    common: "rgb(168, 168, 168)", // light grey
-    normal: "blue", 
-    intermediate: "rgb(69, 150, 255)", // light blue
-    advanced: "rgb(50, 205, 219)", // cyan
-    rare: "green",
-    epic: "red",
-    legendary: "orange",
-    unique: "purple",
-    growth: "#e2abac", // pink?
-    quest: "gold",
-}
+  junk: "rgb(109, 109, 109)", // grey
+  common: "rgb(168, 168, 168)", // light grey
+  normal: "blue",
+  intermediate: "rgb(69, 150, 255)", // light blue
+  advanced: "rgb(50, 205, 219)", // cyan
+  rare: "green",
+  epic: "red",
+  legendary: "orange",
+  unique: "purple",
+  growth: "#e2abac", // pink?
+  quest: "gold",
+};
 
 export const textColors = {
-    blue: "#0d89eb",
-    lightBlue: "#2CA8FF",
-    darkBlue: "#161b21",
-    red: "#f92b00",
-    lightRed: "#ff5745",
-    green: "#54cc4f",
-    offWhite: '#CCCCCC',
-    offBlack: '#1A1A1A',
-    grey: '#474747',
-    electricLime: '#c1ff00',
-}
+  blue: "#0d89eb",
+  lightBlue: "#2CA8FF",
+  darkBlue: "#161b21",
+  red: "#f92b00",
+  lightRed: "#ff5745",
+  green: "#54cc4f",
+  offWhite: "#CCCCCC",
+  offBlack: "#1A1A1A",
+  grey: "#474747",
+  electricLime: "#c1ff00",
+  mana: "#382871",
+  fire: "",
+  metal: "",
+  plant: "",
+  water: "",
+  earth: "",
+  lightning: "",
+  ice: "",
+  wind: "",
+  shadow: "",
+  light: "",
+  sound: "",
+  poison: "",
+  time: "",
+  gravity: "",
+  portal: "",
+  spirit: "",
+  blood: "",
+  none: "",
+};
