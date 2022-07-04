717d76ec5a9870be7844c3b0e21cbf74f5e823c7
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2022-07-04 14:55:10 +0900

    Update places.js

--- src/components/elements/places.js
+++ src/components/elements/places.js
@@ -1,13 +1,202 @@
 export const placeList = {
     Alden: {
         name: "Alden",
-        stats: {},
-        team: "",
-        self: "",
+        "full name": "Grand Duchy of Alden",
+        government: "Grand Duchy",
+
         images: [],
         notes:
           "",
-        tier: "",
         disc: "",
       },
 }
+// ### Countries:
+// Rename 
+// Leika - Shogunate of Niyosho,
+// Huonhia - Herkikia League
+// Hatalia - Naj'es
+// Chalbia - Solzi
+// Mifuegan League - United Provinces of Oflralensa
+
+// Grand Duchy of Alden
+// Republic of Heilerbach
+// United Provinces of Oflralensa
+// Pocotcotco
+// Kingdom of Naj'es
+// Shogunate of Niyosho
+// Herkikia League
+// Solzi
+
+// Granduchy - elected grand duke from dukes.
+// principality - ruled by a prince 
+// kingdom - king
+// republic - representatives from each place - all elected also
+// Places joined together though alliance? to act as one unit under one representative
+// pocotcotco ?
+// Solzi ?
+
+// Normal in the world to reach level 100, but few reset because the chances of unlocking anything new is little and it leaves you open to attack while you level back up. The advanced classes play a large role in the old lands but in the new world people are leveling up less so getting to 100 is a big deal.
+
+// *** notes on the countries *** 
+
+// Alden (German) - 🎄
+// Military - Strong individuals, strong together, strong military, medium navy
+// Diplomacy - None, mostly negative
+// Religion -  
+// Culture - Warlike, military focused, Mostly stable for now.
+// Ambition - Expansion for living space
+// Stability - Mostly stable for now.
+// Secrets -
+// Food - Fertile land, but too focused on military to make good use of it
+// Economy type -
+// Unique Unit - Witches
+// Places of Interest - Kustet (Salt mining town)
+
+// Republic (EU?Roman) 🏛
+// Military - Varied individuals from states, strong together, strong military, weak navy
+// Diplomacy - Good
+// Religion - Varied
+// Culture - Varied
+// Ambition - Not to fall apart
+// Stability - Unstable now, use to be stable.
+// Secrets -
+// Food - Fertile land in the east, less fertile to the south and west.
+// Economy type -
+// Unique Unit - Throwing Axes
+// Places of Interest - Baberg Bridge, Siklad Gold mine, Furlin salt mine, Horod iron mine town.
+
+// League (Elf) 🧝‍♀️
+// Military - Medium individuals, strong together but only one species, strong navy, medium military (use to be weak)
+// Diplomacy - None, mostly negative
+// Religion -
+// Culture -
+// Ambition - Enslave the other species
+// Stability - Mostly stable
+// Secrets -
+// Food -
+// Economy type -
+// Unique Unit - 
+
+// Pocotcotco (Aztec use to be Mayan) 🦅
+// Military - Weak individuals, strong heros?, weak military, no navy
+// Diplomacy - Mostly negative
+// Religion - Old gods (Aztec)
+// Culture - Traditional, unchanging
+// Ambition - Lead their people back to ruling the continent again (reality - try not to become enslaved)
+// Stability - Unknown
+// Secrets - Human sacrifice
+// Food - Fertile land, but too focused on traditional ways to make good use of it
+// Economy type -
+// Unique Unit - Eagle Warrior
+
+// Kingdom of Naj'es (Empire) 🦎
+// Military - weak individuals, strong together, many varied units with a strong core, strong military, weak navy
+// Diplomacy - Arrogant but rational
+// Religion - State religion - , split off religions -
+// Culture - Mostly .. , also
+// Ambition - Depends on the ruler
+// Stability - Stable under one ruler, unstable during changes.
+// Secrets - The current king is weak and it is the queen who has been fighting dressed as him
+// Food - Mostly desert, imports food from others
+// Economy type -
+// Unique Unit - 
+
+// Shogunate of Niyosho (Japan) ⛩
+// Military - weak individuals, strong together, strong military, Strong navy
+// Diplomacy - Medium, while they do not strongly anger anyone they are hardly friendly
+// Religion - Shinto
+// Culture - They build beautiful things, enjoy beauty, but cast out the ugly.
+// Ambition - Become isolationist
+// Stability - Stable
+// Secrets - Racist
+// Food - Fertile land makes good use of it
+// Economy type -
+// Unique Unit - Samurai
+// Places of Interest - Aesemria silver mine town
+
+// Herkikia League - essos, lyric speaking? - free city types ⚔
+// Military - Strong individuals, strong core, weak auxiliary branches, would be strong overall if they would work together
+// Diplomacy - Too focused internally, suits the Niyosho just fine.
+// Religion - Many gods.
+// Culture - Each city has it's own differences but generally they are about their peoples freedom while enslaving others.
+// Ambition - lack of...
+// Stability - Unstable until there is an external threat
+// Secrets -
+// Food - Fertile land, but worked by the slaves
+// Economy type -
+// Unique Unit - Sacred Band
+
+// Solzi- Chalbia (Port) ⚓
+// Military - Average individuals, strong navy, weak land forces, but very rich
+// Diplomacy - Good with most, it's all about trade money and friends.
+// Religion -
+// Culture - Make more money!!!
+// Ambition - More money.
+// Stability - mostly Stable internally, strong neighbor's
+// Secrets -
+// Food - Imports  
+// Economy type -
+// Unique Unit - Pirate
+
+// **Minor**
+
+// Dwarves - Weak with magic, not much food or water, low population, strong melee fighters.
+
+// Desert (Monsters) - They are monsters. It's hard to say.
+
+// \*/}
+
+// ### Eras/History:
+
+// {/* Pre-Dao, Dao-era, Immortal-era, (Huge beats, men struggling, the island is cleansed of men), (Foundation of empires in the east and this continent is invaded by the humans for the first time since the immortals), (Men overcome beasts and empires start here and in the east the religious wars start), Pyramid (Kʼawiil) Empires of men, Pocotcotcan (The one empire), Witch (Rebellion), Now. */}
+// {/* Pre-Dao, Dao, Immortal, Dark-Age, (Dawn Age) Great Migration, Warning Kingdoms Period, Pyramid (The One Kingdom), Pocotcotcan Empire, Witch, Now. */}
+
+// Blood tax, one child from 40 house holds. (Ottomans)
+
+// It has been at least 107 years since the fall of Pocotcotco as an empire.
+// The Elves and the republic have had at least 2 famous battles against each other.
+
+// The new world is in a balance. 
+// The Republic fractures from invasions by the Elves, late empire problems, uninterested ruling class, Alden invasion, Sea people sabotage.
+// Alden invades Herkikia League, they both collapse under the sea peoples invasion.
+// The Kingdom having become the Empire forms a deal with Niyosho but are both destroyed by the sea people
+// Our characters flee though the desert/Solzi into Pocotcotco, who are under invasion by the Elves.
+// Who again fall before the sea people in ruin.
+
+
+// Miasma, "a contagious power... that has an independent life of its own. Until purged by the sacrificial death of the wrongdoer, society would be chronically infected by catastrophe".
+
+// Map: https://azgaar.github.io/Fantasy-Map-Generator/
+// Towns: 
+// https://watabou.github.io/city-generator/?size=18&seed=272082858&name=Todtenwei&population=2265&greens=0&farms=1&citadel=1&urban_castle=0&plaza=0&temple=0
+
+// Todtenwei - 272082858
+// Steinauhau - 5628570730133
+// Alden - 839360670
+// Savigal - 640470293
+
+// Migration of humans, Starting from the core.
+// 1) [Meroyri] - Core + Wood-Elves (Europe) + Tundra 
+// 2) [Lóng_zhī_guó] - (China) + Tundra 
+// 3) [Rayndr] - (Bridge, Al Bhed) - Safer, route.
+// 4) かざんとう -> あばんそふ [Abansofu]  (Xuanzang / Journey to the West) - (Indians) - Large single continent. (Deep water)
+// 5) [Lith_eru] - (Large new continent, sand world) [the location of the immortals war, their bodies made the desert, their power changed to mana and made the first humans]
+// 6) [Charia] (New world) Large single content (Story)
+
+// ### Towns and cities
+
+// Bathhouse
+// forge / armory 
+// Stores
+// Ministries (finance, defence)
+// backgrounds
+// Skill instructors ?
+// Alliance teleport
+// Auction house
+// town teleport
+// Assembly hall
+// Citizen registation
+// bars
+// Dueling 
+// Arena
+// Castle wars
