50cc8fa9cbf89036d19c76253158048e69de3468
ShaunGruuw <111718169+ShaunGruuw@users.noreply.github.com>
2023-08-10 23:15:32 +0900

    Update Vampire.js

--- src/components/Monad/Vampire.js
+++ src/components/Monad/Vampire.js
@@ -281,15 +281,15 @@ export function SpellFinder() {
       <div>
         <h4>A generic player will have:</h4>
         <ul>
-          <li>1 clearing skill</li>
-          <li>1 single target skill</li>
-          <li>1/2 movement skill(s)</li>
-          <li>1 main defensive skill</li>
-          <li>other defensive skills</li>
-          <li>Auras/Stances</li>
-          <li>Passive skills</li>
-          <li>Utility skills</li>
-          <li>Skills that link to the main skills in the tree, e.g. when summoning plant monsters you need to be able to germinate, grow and harvest seeds.</li>
+          <li>1 clearing skill<Hover word={"Dhampir"} /></li>
+          <li>1 single target skill<Hover word={"Dhampir"} /></li>
+          <li>1/2 movement skill(s)<Hover word={"Dhampir"} /></li>
+          <li>1 main defensive skill<Hover word={"Dhampir"} /></li>
+          <li>other defensive skills<Hover word={"Dhampir"} /></li>
+          <li>Auras/Stances<Hover word={"Dhampir"} /></li>
+          <li>Passive skills<Hover word={"Dhampir"} /></li>
+          <li>Utility skills<Hover word={"Dhampir"} /></li>
+          <li>Skills that link to the main skills in the tree, e.g. when summoning plant monsters you need to be able to germinate, grow and harvest seeds.<Hover word={"Dhampir"} /></li>
         </ul>
         <div>
           With that in mind, the starting three classes:
@@ -331,7 +331,7 @@ export function SpellFinder() {
 
           team bonus for fighter/rogue/mage
 
-          <li>Classes/Species</li>
+          <li>Classes/Species<Hover word={"Dhampir"} /></li>
           Blue - 0 - 50 +2, 51 - 100 + 3
           Green - 0 - 50 + 3, 51 - 100 +2
           Red - 0-50 +3, 51 - 100 +3
@@ -347,18 +347,18 @@ export function SpellFinder() {
         </div>
         <div>
           ### Stats:
-          <li>Vitality</li> Increases the recovery rate of HP. Total HP. Basic resistances.
-          <li>Endurance</li> Increases resistance to physical damage, reduces physical damage taken (%), increases stamina.
-          <li>Willpower</li> Increases resistance to magic, reduces magic damage taken (%), resistance to mental attacks. [spell duration (efficiency? Duration max?)]
+          <li>Vitality<Hover word={"Dhampir"} /></li> Increases the recovery rate of HP. Total HP. Basic resistances.
+          <li>Endurance<Hover word={"Dhampir"} /></li> Increases resistance to physical damage, reduces physical damage taken (%), increases stamina.
+          <li>Willpower<Hover word={"Dhampir"} /></li> Increases resistance to magic, reduces magic damage taken (%), resistance to mental attacks. [spell duration (efficiency? Duration max?)]
 
-          <li>Magic</li> increases Magical Attack Power and maximum MP and recovery rate.
-          <li>Strength</li> Increased Physical Attack Power and the weight behind each attack. Minor: Increased storage capacity. Reduce damage taken during a successful block. Increased resistance to falling. Increased movement speed.
+          <li>Magic<Hover word={"Dhampir"} /></li> increases Magical Attack Power and maximum MP and recovery rate.
+          <li>Strength<Hover word={"Dhampir"} /></li> Increased Physical Attack Power and the weight behind each attack. Minor: Increased storage capacity. Reduce damage taken during a successful block. Increased resistance to falling. Increased movement speed.
 
-          <li>Dexterity (Reflexes)</li>Increases Attack Speed, dodge change, movement on rough terrain. Reduces skills cool down.
+          <li>Dexterity (Reflexes)<Hover word={"Dhampir"} /></li>Increases Attack Speed, dodge change, movement on rough terrain. Reduces skills cool down.
 
-          <li>Sense</li> Range of attacks, accuracy. Trap Detect (All of the senses, hearing, taste, touch, smell.) Seeing through smoke screens and illusions. Seeing weakness and injuries in your opponent (CRIT), Range of summons / familia can move from you.
+          <li>Sense<Hover word={"Dhampir"} /></li> Range of attacks, accuracy. Trap Detect (All of the senses, hearing, taste, touch, smell.) Seeing through smoke screens and illusions. Seeing weakness and injuries in your opponent (CRIT), Range of summons / familia can move from you.
 
-          <li>Charisma</li> Increases NPC likability, command points
+          <li>Charisma<Hover word={"Dhampir"} /></li> Increases NPC likability, command points
         </div>
         <div>
           ### Levels:
@@ -601,32 +601,33 @@ export default function Vampire() {
       <h1>Vampire Ranks</h1>
       <SubTitle>Vampires</SubTitle>
       <ul>
-        <li>Vampire Noble + Ranks</li>
-        <li>Vampire Knight</li>
-        <li>Dhampir - Human/Vampire</li>
-        <li>Halfling</li>
-        <li>Royal Vampires</li>
-        <li>Vampire</li>
-        <li>Vampire Fledgling</li>
-        <li>Blooded Vampire</li>
-        <li>Vampire Nightstalker</li>
-        <li>Vampire Lord</li>
-        <li>Vampire progenitor</li>
-        <li>Vampire Princess</li>
-        <li>Vampire Prince</li>
-        <li>Vampire Bride</li>
+        <li><Hover word={"Vampire Noble"} /></li>
+        <li><Hover word={"Vampire Knight"} /></li>
+        <li><Hover word={"Dhampir"} /></li>
+        <li><Hover word={"Halfling"} /></li>
+        <li><Hover word={"Royal Vampires"} /></li>
+        <li><Hover word={"Vampire"} /></li>
+        <li><Hover word={"Vampire Fledgling"} /></li>
+        <li><Hover word={"Blooded Vampire"} /></li>
+        <li><Hover word={"Vampire Nightstalker"} /></li>
+        <li><Hover word={"Vampire Lord"} /></li>
+        <li><Hover word={"Vampire progenitor"} /></li>
+        <li><Hover word={"Vampire Princess"} /></li>
+        <li><Hover word={"Vampire Prince"} /></li>
+        <li><Hover word={"Vampire Bride"} /></li>
       </ul>
       <SubTitle>Sub-classes</SubTitle>
       <ul>
-        <li>Ghoul</li>
-        <li>Wight</li>
-        <li>Blood Fairy</li>
-        <li>Strigoi</li>
-        <li>Hannya</li>
-        <li>Draugr</li>
-        <li>Banshee</li>
-        <li>vampiric beasts</li>
-        {/* Thrall, Lurker */}
+        <li><Hover word={"Ghoul"} /></li>
+        <li><Hover word={"Wight"} /></li>
+        <li><Hover word={"Blood Fairy"} /></li>
+        <li><Hover word={"Strigoi"} /></li>
+        <li><Hover word={"Hannya"} /></li>
+        <li><Hover word={"Draugr"} /></li>
+        <li><Hover word={"Banshee"} /></li>
+        <li><Hover word={"vampiric beasts"} /></li>
+        <li><Hover word={"Thrall"} /></li>
+        <li><Hover word={"Lurker"} /></li>
       </ul>
     </div>
   );
