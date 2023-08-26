f934ed2b0762b627a8c461c982eee6fe1573fd96
ottotsuma <51743227+ottotsuma@users.noreply.github.com>
2023-08-26 15:41:52 +0900

    Update Ch3-Banderedam.js

--- src/components/Monad/Ch3-Banderedam.js
+++ src/components/Monad/Ch3-Banderedam.js
@@ -2,6 +2,7 @@ import React from "react";
 import Item from './../elements/item'
 import { Content } from "./styles";
 import Hover from "../elements/hover";
+import { Otto, Leon } from "./CharacterFonts";
 
 // Dead girl, corps fish 
 function Banderedam(props) {
