e3a5b517cd4ca056dbad1bdfebd215887f6b3c2e
ShaunGruuw <111718169+ShaunGruuw@users.noreply.github.com>
2025-02-01 22:48:31 +0900

    gifs require

--- src/components/color.js
+++ src/components/color.js
@@ -1,21 +1,21 @@
-import Empty from "./../assets/empty.gif";
+const Empty = require("./../assets/empty.gif");
 export function imageError(ev) {
   ev.target.src = Empty
 }
 
 // These colors are local to this file
 const colors = {
-    blue: "#0d89eb",
-    lightBlue: "#2CA8FF",
-    darkBlue: "#161b21",
-    red: "#f92b00",
-    lightRed: "#ff5745",
-    green: "#54cc4f",
-    offWhite: '#CCCCCC',
-    offBlack: '#1A1A1A',
-    grey: '#474747',
-    electricLime: '#c1ff00',
-    lightBackground: `linear-gradient(0deg, rgba(34,89,195,0.7) 9%, rgba(45,253,209,0.3) 100% )`,
+  blue: "#0d89eb",
+  lightBlue: "#2CA8FF",
+  darkBlue: "#161b21",
+  red: "#f92b00",
+  lightRed: "#ff5745",
+  green: "#54cc4f",
+  offWhite: '#CCCCCC',
+  offBlack: '#1A1A1A',
+  grey: '#474747',
+  electricLime: '#c1ff00',
+  lightBackground: `linear-gradient(0deg, rgba(34,89,195,0.7) 9%, rgba(45,253,209,0.3) 100% )`,
 }
 
 export const BackgroundColor = `
@@ -27,15 +27,15 @@ linear-gradient(
 `
 
 export const HomeColors = {
-    LinkHover: colors.lightBlue,
-    Button: colors.blue,
-    ButtonHover: colors.lightBlue,
-    circle1: `linear-gradient(
+  LinkHover: colors.lightBlue,
+  Button: colors.blue,
+  ButtonHover: colors.lightBlue,
+  circle1: `linear-gradient(
         0deg,
         rgba(255, 0, 221, 0.7),
         rgba(255, 255, 255, 0.3)
       );`,
-    circle2: `linear-gradient(
+  circle2: `linear-gradient(
         0deg,
         rgba(255, 0, 221, 0.7),
         rgba(255, 255, 255, 0.3)
@@ -43,5 +43,5 @@ export const HomeColors = {
 }
 
 export const NavColors = {
-    NavBar: colors.blue,
+  NavBar: colors.blue,
 }
--- src/components/video.js
+++ src/components/video.js
@@ -1,56 +1,57 @@
-
-import sheep1 from "./../assets/sheep1.gif";
-import sheep2 from "./../assets/sheep2.gif";
-import sheep3 from "./../assets/sheep3.gif";
-import sheep4 from "./../assets/sheep4.gif";
-import sheep6 from "./../assets/sheep6.gif";
-import sheep7 from "./../assets/sheep7.gif";
-import sheep8 from "./../assets/sheep8.gif";
-import sheep9 from "./../assets/sheep9.gif";
-import sheep10 from "./../assets/sheep10.gif";
 import styled from "styled-components";
-import birthday from "./../assets/sheep5.gif";
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+const sheepImages = {
+  sheep1: require("./../assets/sheep1.gif"),
+  sheep2: require("./../assets/sheep2.gif"),
+  sheep3: require("./../assets/sheep3.gif"),
+  sheep4: require("./../assets/sheep4.gif"),
+  sheep6: require("./../assets/sheep6.gif"),
+  sheep7: require("./../assets/sheep7.gif"),
+  sheep8: require("./../assets/sheep8.gif"),
+  sheep9: require("./../assets/sheep9.gif"),
+  sheep10: require("./../assets/sheep10.gif"),
+  birthday: require("./../assets/sheep5.gif"),
+};
 
 function getRandomArbitrary(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function Video() {
-  let array = [sheep1, sheep2, sheep3, sheep4, sheep6, sheep7, sheep8, sheep9, sheep10];
-  let number = getRandomArbitrary(0, array.length - 1);
-  let sheep = array[number];
-  if (
-    new Date().toDateString().split(" ")[1] === "Jun" &&
-    new Date().toDateString().split(" ")[2] === "20"
-  ) {
-    return (
-      <div className="App">
-        <Sheep src={birthday} alt="Birthday" type="gif" />
-      </div>
-    );
-  } else {
-    return (
-      <div className="App">
-        <Sheep src={sheep} alt="Sheep" type="gif" />
-      </div>
-    );
-  }
+  const [sheep, setSheep] = useState(null);
+
+  useEffect(() => {
+    let selectedSheep = sheepImages[`sheep${getRandomArbitrary(1, 10)}`];
+
+    if (new Date().getMonth() === 5 && new Date().getDate() === 20) {
+      selectedSheep = sheepImages.birthday;
+    }
+
+    setSheep(selectedSheep);
+  }, []);
+
+  if (!sheep) return null;
+
+  return (
+    <div className="Sheep">
+      <SheepImg src={sheep} alt="Sheep" />
+    </div>
+  );
 }
+
 export default Video;
 
-const Sheep = styled.img`
-position: absolute;
-width: 100px;
-height: 100px;
-z-index: 1;
-left: 53%;
-top: 81%;
+const SheepImg = styled.img`
+  position: absolute;
+  min-height: 100px; /* Reserve space */
+  width: 100px;
+  height: 100px;
+  z-index: 1;
+  left: 53%;
+  top: 81%;
 
-@media screen and (max-width: 730px) {
-  display: none;
-}
+  @media screen and (max-width: 730px) {
+    display: none;
+  }
 `;
-
